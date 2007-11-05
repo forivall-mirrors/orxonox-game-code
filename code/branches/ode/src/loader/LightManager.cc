6b9ed404f9293d3dc57022edecdb61a62e57271b
nicolasc <nicolasc@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2007-11-05 12:24:58 +0000

    created ode branche from trunk

---
+++ code/branches/ode/src/loader/LightManager.cc
@@ -0,0 +1,28 @@
+
+#include "../xml/xmlParser.h"
+#include "LightManager.h"
+
+namespace light
+{
+	LightManager::LightManager()
+	{
+		vector<Light> this->elements_ = new vector<Light>;
+	}
+	
+	void LightManager::loadParams(XMLNode xml)
+	{
+		if (!xml.isEmpty())
+		{
+			int nLights = xml.nChildNode("light");
+			for (int i=0; i<nLights;i++)
+			{
+				Light l = new Light(xml.getChildNode("light",i));
+				this->elements_.append(l);
+			}
+		}		
+		
+	}	
+		
+	
+}
+
