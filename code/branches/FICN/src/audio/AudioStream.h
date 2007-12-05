f3188b06b546dd5dd440f4942000c05e0704d287
nicolape <nicolape@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2007-12-05 16:24:16 +0000

    Added ogg streamer class

---
+++ code/branches/FICN/src/audio/AudioStream.h
@@ -0,0 +1,41 @@
+#ifndef __ogg_h__
+#define __ogg_h__
+
+#include "AudioIncludes.h"
+
+namespace audio
+{
+	#define BUFFER_SIZE (4096 * 4)
+	
+	class AudioStream
+	{
+	    public:
+	
+	        void open(string path);
+	        void release();
+	        void display();
+	        bool playback();
+	        bool playing();
+	        bool update();
+	
+	    protected:
+	
+	        bool stream(ALuint buffer);
+	        void empty();
+	        void check();
+	        std::string errorString(int code);
+	
+	    private:
+	
+	        FILE*           oggFile;
+	        OggVorbis_File  oggStream;
+	        vorbis_info*    vorbisInfo;
+	        vorbis_comment* vorbisComment;
+	
+	        ALuint buffers[2];
+	        ALuint source;
+	        ALenum format;
+	};
+}
+
+#endif // __ogg_h__
