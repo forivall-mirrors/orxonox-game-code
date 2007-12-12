8341671d2c0b5098116a0731d935c8acbf8823f1
bknecht <bknecht@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2007-12-12 11:42:30 +0000

    got my work to compile (it's all hacked together and I dunno if it runs)  - orxonox.h           we got now a real .h file for our main class  - orxonox.cc         this is the all hacked class. We/I have to strip that clean  - orxonoxOld.cc    "You did something wrong, the old implementation was SOO much better"  - graphicsEngine  we should have a wrapper around ogre so we have a real graphics module  - main.cc              created a main again doing all the platform depending stuff in there.. (hope this works)
    think that's all...

---
+++ code/branches/FICN/src/orxonox/main.cc
@@ -0,0 +1,103 @@
+/*
+ *   ORXONOX - the hottest 3D action shooter ever to exist
+ *
+ *
+ *   License notice:
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *   Author:
+ *      Benjamin Knecht <beni_at_orxonox.net>, (C) 2007
+ *   Co-authors:
+ *      ...
+ *
+ */
+
+ /**
+ @file  main.cc
+ @brief main file handling most of the machine specific code
+  */
+
+#include <OgrePlatform.h>
+#include <OgreException.h>
+
+
+#include "orxonox.h"
+
+using namespace orxonox;
+using namespace Ogre;
+#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
+#include <CoreFoundation/CoreFoundation.h>
+
+// This function will locate the path to our application on OS X,
+// unlike windows you can not rely on the curent working directory
+// for locating your configuration files and resources.
+             std::string macBundlePath()
+{
+  char path[1024];
+  CFBundleRef mainBundle = CFBundleGetMainBundle();
+  assert(mainBundle);
+
+  CFURLRef mainBundleURL = CFBundleCopyBundleURL(mainBundle);
+  assert(mainBundleURL);
+
+  CFStringRef cfStringRef = CFURLCopyFileSystemPath( mainBundleURL, kCFURLPOSIXPathStyle);
+  assert(cfStringRef);
+
+  CFStringGetCString(cfStringRef, path, 1024, kCFStringEncodingASCII);
+
+  CFRelease(mainBundleURL);
+  CFRelease(cfStringRef);
+
+  return std::string(path);
+}
+#endif
+
+#if OGRE_PLATFORM == PLATFORM_WIN32 || OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+
+             INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT)
+#else
+
+             int main(int argc, char **argv)
+#endif
+{
+  try
+  {
+    Orxonox* orx = Orxonox::getSingleton();
+#if OGRE_PLATFORM == OGRE_PLATFORM_APPLE
+    orx->init(argc, argv, macBundlePath());
+    orx->start();
+#else
+    orx->init(argc, argv, "");
+    orx->start();
+#endif
+
+  }
+  catch(Ogre::Exception& e)
+  {
+#if OGRE_PLATFORM == PLATFORM_WIN32 || OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+    MessageBoxA(NULL, e.getFullDescription().c_str(), "An exception has occurred!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+    fprintf(stderr, "An exception has occurred: %s\n",
+            e.getFullDescription().c_str());
+    return 1;
+#endif
+  }
+
+  return 0;
+}
+
---
+++ code/branches/FICN/src/orxonox/orxonox.h
@@ -0,0 +1,60 @@
+/**
+ @file  orxonox.h
+ @brief Main Orxonox Class File
+ @author Benjamin Knecht <beni_at_orxonox.net>
+ */
+
+#ifndef ORXONOX_H
+#define ORXONOX_H
+
+//#include <OgreSingleton.h>
+#include <OgreSceneManager.h>
+
+#include <OIS/OIS.h>
+
+#include "graphicsEngine.h"
+#include "../loader/LevelLoader.h"
+#include "../audio/AudioManager.h"
+
+
+// TODO: Orxonox should maybe derive from BaseObject
+//! Orxonox singleton class
+namespace orxonox {
+
+  class OrxListener;
+
+  class Orxonox
+  {
+    public:
+      void init(int argc, char **argv, std::string path);
+      void start();
+      // not sure if this should be private
+      void die(/* some error code */);
+      static Orxonox* getSingleton();
+      inline SceneManager* getSceneManager() { return ogre_->getSceneManager(); };
+    private:
+      Orxonox();
+      virtual ~Orxonox();
+      void serverInit(std::string path);
+      void clientInit(std::string path);
+      void standalone(std::string path);
+      void createScene(void);
+      void setupScene();
+      void setupInputSystem();
+      void createFrameListener();
+      void startRenderLoop();
+    private:
+
+      //static Orxonox*       singletonRef_;
+      GraphicsEngine*       ogre_;      //!< our dearest graphics engine <3
+      std::string           dataPath_;  //!< path to data
+      loader::LevelLoader*  loader_;    //!< level loader builds the scene
+      audio::AudioManager*  auMan_;     //!< audio manager
+      static Orxonox*       singletonRef_;
+      OIS::Keyboard*        keyboard_;
+      OIS::Mouse*           mouse_;
+      OIS::InputManager*    inputManager_;
+      OrxListener*          frameListener_;
+  };
+}
+#endif /* ORXONOX_H */
