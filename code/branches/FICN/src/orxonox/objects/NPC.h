5085cde204cf8b0852db2fd6f5614c783dacdbf0
bknecht <bknecht@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2007-12-17 21:37:02 +0000

    now really npc class added

---
+++ code/branches/FICN/src/orxonox/objects/NPC.h
@@ -0,0 +1,22 @@
+/**
+  @file NPC.h
+  @brief AI using WorldEntity
+  @author Benjamin Knecht <beni_at_orxonox.net>
+ */
+
+#ifndef MODULE_NPC_H
+#define MODULE_NPC_H
+
+// includes
+#include "WorldEntity.h"
+
+namespace orxonox {
+
+  class NPC : public WorldEntity
+  {
+    NPC();
+    virtual ~NPC();
+  };
+
+}
+#endif
