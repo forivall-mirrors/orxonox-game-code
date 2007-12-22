28aa5c2541353104a168d6ded4323b3b27eeead6
rgrieder <rgrieder@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2007-12-22 15:45:34 +0000

    - completed last svn update (delete and add to rename first letter) - modified CMLs in oroxnox accordingly - added visual studio files

---
+++ code/branches/FICN/src/orxonox/objects/weapon_system/Bullet.h
@@ -0,0 +1,51 @@
+/*
+ *   ORXONOX - the hottest 3D action shooter ever to exist
+ *
+ *
+ *   License notice:
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ *   Author:
+ *      Reto Grieder
+ *   Co-authors:
+ *      ...
+ *
+ */
+
+
+#ifndef BULLET_H
+#define BULLET_H
+
+#include <OgrePrerequisites.h>
+
+#include "orxonox/OrxonoxPrereqs.h"
+
+#include "../Model.h"
+
+namespace orxonox {
+  class Bullet : public Model
+  {
+  public:
+	  Bullet();
+	  ~Bullet();
+
+    virtual void tick(float dt) { }
+
+    inline virtual void loadParams(TiXmlElement* xmlElem) { Model::loadParams(xmlElem); };
+ };
+}
+
+#endif /* BULLET_H */
