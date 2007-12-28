a2ff2a488cb6ed9a605cc39318c7e90b57222541
landauf <landauf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2007-12-28 22:23:08 +0000

    exclusion for mingw

--- code/branches/FICN/src/orxonox/Main.cc
+++ code/branches/FICN/src/orxonox/Main.cc
@@ -161,7 +161,7 @@ std::cout << "    <Model position=\"" << x << "," << y << "," << z << "\" scale=
 #endif
     }
     catch (Ogre::Exception& e) {
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32 && !defined( __MINGW32__ )
       MessageBoxA(NULL, e.getFullDescription().c_str(),
             "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
 #else
