723b93a5ab7a1f0b080930ba6a24de51507c63d7
nicolasc <nicolasc@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2007-12-30 22:55:35 +0000

    house cleaning in spaceship.h

--- code/branches/FICN/src/orxonox/objects/SpaceShip.h
+++ code/branches/FICN/src/orxonox/objects/SpaceShip.h
@@ -23,29 +23,10 @@ namespace orxonox
             void setConfigValues();
             virtual void loadParams(TiXmlElement* xmlElem);
             virtual void tick(float dt);
-/*
-            void moveForward(float moveForward);
-            void rotateUp(float rotateUp);
-            void rotateDown(float rotateDown);
-            void rotateRight(float rotateRight);
-            void rotateLeft(float rotateLeft);
-            void loopRight(float loopRight);
-            void loopLeft(float loopLeft);
-            void brakeForward(float brakeForward);
-            void brakeRotate(float brakeRotate);
-            void brakeLoop(float brakeLoop);
-            void maxSpeedForward(float maxSpeedForward);
-            void maxSpeedRotateUpDown(float maxSpeedRotateUpDown);
-            void maxSpeedRotateRightLeft(float maxSpeedRotateRightLeft);
-            void maxSpeedLoopRightLeft(float maxSpeedLoopRightLeft);
-*/
+
             bool mouseMoved(const OIS::MouseEvent &e);
             bool mousePressed(const OIS::MouseEvent &e, OIS::MouseButtonID id);
             bool mouseReleased(const OIS::MouseEvent &e, OIS::MouseButtonID id);
-
-            void doNothing() {
-            int a = 0;    //FIXME unused var
-            }
 
 
         private:
@@ -90,42 +71,6 @@ namespace orxonox
             float mouseX_;
             float mouseY_;
 
-/*
-            float moveForward_;
-            float rotateUp_;
-            float rotateDown_;
-            float rotateRight_;
-            float rotateLeft_;
-            float loopRight_;
-            float loopLeft_;
-            float brakeForward_;
-            float brakeRotate_;
-            float brakeLoop_;
-
-            float speedForward_;
-            float speedRotateUpDown_;
-            float speedRotateRightLeft_;
-            float speedLoopRightLeft_;
-
-            float maxSpeedForward_;
-            float maxSpeedRotateUpDown_;
-            float maxSpeedRotateRightLeft_;
-            float maxSpeedLoopRightLeft_;
-
-            float accelerationForward_;
-            float accelerationRotateUpDown_;
-            float accelerationRotateRightLeft_;
-            float accelerationLoopRightLeft_;
-
-            float speed;
-            float rotate;
-            float loop;
-            float mouseY;
-            float mouseX;
-            float maxMouseX;
-            float minMouseX;
-            bool moved;
-*/
             int emitterRate_;
     };
     ExportClass(SpaceShip, Orxonox);
