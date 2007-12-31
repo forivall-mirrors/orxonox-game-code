8cd621a3f5f05f5c7209677398d8f5875a585740
landauf <landauf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2007-12-31 03:39:53 +0000

    license notice

--- code/branches/FICN/src/util/substring.cc
+++ code/branches/FICN/src/util/substring.cc
@@ -1,19 +1,28 @@
 /*
-   orxonox - the future of 3D-vertical-scrollers
-
-   Copyright (C) 2004 orx
-
-   This program is free software; you can redistribute it and/or modify
-   it under the terms of the GNU General Public License as published by
-   the Free Software Foundation; either version 2, or (at your option)
-   any later version.
-
-   ### File Specific:
-   main-programmer: Christian Meyer
-   co-programmer: Benjamin Grauer
-
-   2005-06-10: some naming conventions
-
+ *   ORXONOX - the hottest 3D action shooter ever to exist
+ *
+ *
+ *   License notice:
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *   Author:
+ *      Christian Meyer
+ *   Co-authors:
+ *      Benjamin Grauer
+ *
 //
 //  splitLine
 //  STL string tokenizer
@@ -22,8 +31,7 @@
 //  Version 1.0
 //  Copyright (c) 2005 Clemens Wacha. All rights reserved.
 //
-
-*/
+ */
 
 #include "substring.h"
 
