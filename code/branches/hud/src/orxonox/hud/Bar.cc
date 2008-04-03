f9ee421ede61f64cca3ecec75a9ab2f75691903b
chaiy <chaiy@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2008-04-03 12:51:44 +0000

    hallo

--- code/branches/hud/src/orxonox/hud/Bar.cc
+++ code/branches/hud/src/orxonox/hud/Bar.cc
@@ -2,7 +2,7 @@
 *   ORXONOX - the hottest 3D action shooter ever to exist
 *
 *
-*   License notice:
+*   License notice:asdf
 *
 *   This program is free software; you can redistribute it and/or
 *   modify it under the terms of the GNU General Public License
