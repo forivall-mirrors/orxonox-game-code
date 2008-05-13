5e37b9768710a85e9afec2ad8082132b8eef4859
scheusso <scheusso@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2008-05-13 13:22:56 +0000

    creating merge branch

---
+++ code/branches/merge/src/orxonox/objects/Camera.cc
@@ -0,0 +1,102 @@
+/*
+ *   ORXONOX - the hottest 3D action shooter ever to exist
+ *                    > www.orxonox.net <
+ *
+ *
+ *   License notice:
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *   Author:
+ *      Fabian 'x3n' Landau
+ *   Co-authors:
+ *      ...
+ *
+ */
+
+#include "OrxonoxStableHeaders.h"
+#include "Camera.h"
+
+#include <string>
+
+#include <OgreSceneManager.h>
+#include <OgreSceneNode.h>
+#include <OgreRenderWindow.h>
+#include <OgreViewport.h>
+
+#include "tinyxml/tinyxml.h"
+#include "util/SubString.h"
+#include "util/Convert.h"
+#include "util/Math.h"
+#include "core/Debug.h"
+#include "core/CoreIncludes.h"
+#include "GraphicsEngine.h"
+
+namespace orxonox
+{
+    CreateFactory(Camera);
+
+    Camera::Camera()
+    {
+        RegisterObject(Camera);
+    }
+
+    Camera::~Camera()
+    {
+    }
+
+    void Camera::loadParams(TiXmlElement* xmlElem)
+    {
+      Ogre::SceneManager* mgr = GraphicsEngine::getSingleton().getSceneManager();
+
+      if (xmlElem->Attribute("name") && xmlElem->Attribute("pos") && xmlElem->Attribute("lookat") && xmlElem->Attribute("node"))
+      {
+        //    <Camera name="Camera" pos="0,0,-250" lookat="0,0,0" />
+
+        std::string name = xmlElem->Attribute("name");
+        std::string pos = xmlElem->Attribute("pos");
+        std::string lookat = xmlElem->Attribute("lookat");
+
+        Ogre::Camera *cam = mgr->createCamera(name);
+
+        float x, y, z;
+        SubString posVec(xmlElem->Attribute("pos"), ',');
+        convertValue<std::string, float>(&x, posVec[0]);
+        convertValue<std::string, float>(&y, posVec[1]);
+        convertValue<std::string, float>(&z, posVec[2]);
+
+        cam->setPosition(Vector3(x,y,z));
+
+        posVec = SubString(xmlElem->Attribute("lookat"), ',');
+        convertValue<std::string, float>(&x, posVec[0]);
+        convertValue<std::string, float>(&y, posVec[1]);
+        convertValue<std::string, float>(&z, posVec[2]);
+
+        cam->lookAt(Vector3(x,y,z));
+
+        std::string node = xmlElem->Attribute("node");
+
+        Ogre::SceneNode* sceneNode = (Ogre::SceneNode*)mgr->getRootSceneNode()->createChildSceneNode(node); //getChild(node);
+        sceneNode->attachObject((Ogre::MovableObject*)cam);
+
+        // FIXME: unused var
+        //Ogre::Viewport* vp = 
+        GraphicsEngine::getSingleton().getRenderWindow()->addViewport(cam);
+
+
+        COUT(4) << "Loader: Created camera "<< name  << std::endl << std::endl;
+      }
+   }
+}
---
+++ code/branches/merge/src/util/MultiType.h
@@ -0,0 +1,79 @@
+/*
+ *   ORXONOX - the hottest 3D action shooter ever to exist
+ *                    > www.orxonox.net <
+ *
+ *
+ *   License notice:
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *   Author:
+ *      Fabian 'x3n' Landau
+ *   Co-authors:
+ *      ...
+ *
+ *   Inspiration: MultiType by Benjamin Grauer
+ */
+
+#ifndef _MultiType_H__
+#define _MultiType_H__
+
+#include "UtilPrereqs.h"
+
+enum _UtilExport MultiType
+{
+    MT_null,
+    MT_void,
+    MT_int,
+    MT_uint,
+    MT_char,
+    MT_uchar,
+    MT_short,
+    MT_ushort,
+    MT_long,
+    MT_ulong,
+    MT_float,
+    MT_double,
+    MT_longdouble,
+    MT_bool,
+    MT_constchar,
+    MT_string,
+    MT_xmlelement,
+    MT_vector2,
+    MT_vector3,
+    MT_colourvalue,
+    MT_quaternion,
+    MT_degree,
+    MT_radian
+};
+
+union _UtilExport MultiTypeValue
+{
+    void*           void_;
+    int             int_;
+    unsigned int    uint_;
+    char            char_;
+    unsigned char   uchar_;
+    short           short_;
+    unsigned short  ushort_;
+    long            long_;
+    unsigned long   ulong_;
+    float           float_;
+    double          double_;
+    long double     longdouble_;
+    bool            bool_;
+};
+
+#endif /* _MultiType_H__ */
