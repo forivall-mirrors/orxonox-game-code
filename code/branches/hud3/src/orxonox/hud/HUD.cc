828c6c5fd770c6ec64b0ee5a9758b8e73dadab73
FelixSchulthess <FelixSchulthess@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2008-05-19 20:58:51 +0000

    removed old files

--- code/branches/hud3/src/orxonox/hud/HUD.cc
+++ code/branches/hud3/src/orxonox/hud/HUD.cc
@@ -41,62 +41,62 @@
 
 namespace orxonox
 {
-  using namespace Ogre;
+    using namespace Ogre;
 
-  HUD::HUD(int zoom){
-    om = &Ogre::OverlayManager::getSingleton();
+    HUD::HUD(int zoom){
+        om = &Ogre::OverlayManager::getSingleton();
 
-    BarOverlayElementFactory *barOverlayElementFactory = new BarOverlayElementFactory();
-    om->addOverlayElementFactory(barOverlayElementFactory);
+        BarOverlayElementFactory *barOverlayElementFactory = new BarOverlayElementFactory();
+        om->addOverlayElementFactory(barOverlayElementFactory);
 
-    RadarOverlayElementFactory *radarOverlayElementFactory = new RadarOverlayElementFactory();
-    om->addOverlayElementFactory(radarOverlayElementFactory);
+        RadarOverlayElementFactory *radarOverlayElementFactory = new RadarOverlayElementFactory();
+        om->addOverlayElementFactory(radarOverlayElementFactory);
 
-    orxonoxHUD = om->create("Orxonox/HUD");
+        orxonoxHUD = om->create("Orxonox/HUD");
 
-    container = static_cast<Ogre::OverlayContainer*>(om->createOverlayElement("Panel", "Orxonox/HUD/container"));
+        container = static_cast<Ogre::OverlayContainer*>(om->createOverlayElement("Panel", "Orxonox/HUD/container"));
 
-    energyCounter = static_cast<BarOverlayElement*>(om->createOverlayElement("Bar", "energyCounter"));
-    energyCounter->show();
+        energyCounter = static_cast<BarOverlayElement*>(om->createOverlayElement("Bar", "energyCounter"));
+        energyCounter->show();
 
-    speedo = static_cast<BarOverlayElement*>(om->createOverlayElement("Bar", "speedo"));
-    speedo->show();
+        speedo = static_cast<BarOverlayElement*>(om->createOverlayElement("Bar", "speedo"));
+        speedo->show();
 
-    radar = static_cast<RadarOverlayElement*>(om->createOverlayElement("Radar", "radar"));
-    radar->show();
+        radar = static_cast<RadarOverlayElement*>(om->createOverlayElement("Radar", "radar"));
+        radar->show();
 
-    container->show();
-    orxonoxHUD->add2D(container);
-    orxonoxHUD->show();
-    container->setLeft(0.0);
-    container->setTop(0.0);
-    container->setWidth(1.0);
-    container->setHeight(1.0);
-    container->setMetricsMode(Ogre::GMM_RELATIVE);
+        container->show();
+        orxonoxHUD->add2D(container);
+        orxonoxHUD->show();
+        container->setLeft(0.0);
+        container->setTop(0.0);
+        container->setWidth(1.0);
+        container->setHeight(1.0);
+        container->setMetricsMode(Ogre::GMM_RELATIVE);
 
-    energyCounter->init(0.01, 0.95, 0.4, 0.04, container);
-    energyCounter->setValue(1);
+        energyCounter->init(0.01, 0.95, 0.4, 0.04, container);
+        energyCounter->setValue(1);
 
-    speedo->init(0.01, 0.90, 0.4, 0.04, container);
+        speedo->init(0.01, 0.90, 0.4, 0.04, container);
 
-    radar->init(0.5, 0.9, 0.2, container);
-  }
+        radar->init(0.5, 0.9, 0.2, container);
+    }
 
-  void HUD::tick(float dt)
-  {
-      energyCounter->resize();
+    void HUD::tick(float dt)
+    {
+        energyCounter->resize();
 
-      float v = SpaceShip::instance_s->getVelocity().length();
-      float vmax = SpaceShip::instance_s->getMaxSpeed();
-      speedo->setValue(v/vmax);
-      speedo->resize();
+        float v = SpaceShip::instance_s->getVelocity().length();
+        float vmax = SpaceShip::instance_s->getMaxSpeed();
+        speedo->setValue(v/vmax);
+        speedo->resize();
 
-      radar->resize();
-      radar->update();
-  }
+        radar->resize();
+        radar->update();
+    }
 
-  HUD::~HUD(void){
-  }
+    HUD::~HUD(void){
+    }
 }
 
 
