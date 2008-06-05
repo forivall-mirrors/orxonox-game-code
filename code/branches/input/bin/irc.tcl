30c83ef252a17a991acd1eb941a1ea4cb243fc42
rgrieder <rgrieder@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2008-06-05 13:40:13 +0000

    removed network and input branch

--- code/branches/input/bin/irc.tcl
+++
@@ -1,6 +0,0 @@
-proc every {ms body} { eval $body; after $ms [list every $ms $body] }
-package require irk
-set conn [irk::connect datacore $nickname]
-irk::join $conn #orxonox
-every 25 {}
-vwait termination
