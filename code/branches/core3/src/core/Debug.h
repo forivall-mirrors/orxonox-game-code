89b9227ae9cb5b3c274d2bd50fb69401cced1c1d
landauf <landauf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2008-06-10 01:49:24 +0000

    - added debug output level 6 (ultra)  - removed debug output macros PRINT and PRINTF  - used "(false) ? ... : ..." instead of "if (false) ..." to ignore output with too high level because the compiler makes a static evaluation of the trinary ?: operator

--- code/branches/core3/src/core/Debug.h
+++ code/branches/core3/src/core/Debug.h
@@ -50,145 +50,14 @@ extern "C" _CoreExport int getSoftDebugLevel();
 #define ORX_WARNING         2
 #define ORX_INFO            3
 #define ORX_DEBUG           4
-#define ORX_vDEBUG          5
+#define ORX_VERBOSE         5
+#define ORX_ULTRA           6
 
 //definitions
-#define ORX_PRINT_DEBUG_OUTPUT 1 // <-- fix that! should be a configurable setting
+#define ORX_PRINT_DEBUG_OUTPUT 1
+#define ORX_HARD_DEBUG_LEVEL ORX_VERBOSE
 
-#define ORX_HARD_DEBUG_LEVEL ORX_vDEBUG
-//#define ORX_SOFT_DEBUG_LEVEL ORX_WARNING // <-- fix that! should be a configurable setting
-
-///////////////////////////////////////////////////
-/// PRINTF: prints with filename and linenumber ///
-///////////////////////////////////////////////////
-#define PRINTFORX_NONE    PRINTF0
-#define PRINTFORX_ERROR   PRINTF1
-#define PRINTFORX_WARNING PRINTF2
-#define PRINTFORX_INFO    PRINTF3
-#define PRINTFORX_DEBUG   PRINTF4
-#define PRINTFORX_vDEBUG  PRINTF5
-
-#define PRINT_EXEC  printf
-#define COUT_EXEC(x) \
-  orxonox::OutputHandler::getOutStream().setOutputLevel(x)
-
-#ifndef PRINTF
- #if ORX_PRINT_DEBUG_OUTPUT
-
-  #define PRINTF(x) \
-   PRINTF ## x
-
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_ERROR
-   #define PRINTF1 \
-    if (getSoftDebugLevel() >= ORX_ERROR) \
-     printf("Error (in %s, line %d): ", __FILE__, __LINE__), PRINT_EXEC
-  #else
-   #define PRINTF1 if (ORX_NONE) PRINT_EXEC
-  #endif
-
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_WARNING
-   #define PRINTF2 \
-    if (getSoftDebugLevel() >= ORX_WARNING) \
-     printf("Warning (in %s, line %d): ", __FILE__, __LINE__), PRINT_EXEC
-  #else
-   #define PRINTF2 if (ORX_NONE) PRINT_EXEC
-  #endif
-
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_INFO
-   #define PRINTF3 \
-    if (getSoftDebugLevel() >= ORX_INFO) \
-     printf("Info (in %s, line %d): ", __FILE__, __LINE__), PRINT_EXEC
-  #else
-   #define PRINTF3 if (ORX_NONE) PRINT_EXEC
-  #endif
-
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_DEBUG
-   #define PRINTF4 \
-    if (getSoftDebugLevel() >= ORX_DEBUG) \
-     printf("Debug (in %s, line %d): ", __FILE__, __LINE__), PRINT_EXEC
-  #else
-   #define PRINTF4 if (ORX_NONE) PRINT_EXEC
-  #endif
-
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_vDEBUG
-   #define PRINTF5 \
-    if (getSoftDebugLevel() >= ORX_vDEBUG) \
-     printf("vDebug (in %s, line %d): ", __FILE__, __LINE__), PRINT_EXEC
-  #else
-   #define PRINTF5 if (ORX_NONE) PRINT_EXEC
-  #endif
-
- #else /* if ORX_PRINT_DEBUG_OUTPUT */
-  #define PRINTF(x) if (ORX_NONE) PRINT_EXEC
- #endif /* if ORX_PRINT_DEBUG_OUTPUT */
-
- #define PRINTF0 \
-  PRINT_EXEC
-#endif /* ifndef PRINTF */
-
-///////////////////////////////////////////////////
-///  PRINT: just prints output as is            ///
-///////////////////////////////////////////////////
-#define PRINTORX_NONE    PRINT0
-#define PRINTORX_ERROR   PRINT1
-#define PRINTORX_WARNING PRINT2
-#define PRINTORX_INFO    PRINT3
-#define PRINTORX_DEBUG   PRINT4
-#define PRINTORX_vDEBUG  PRINT5
-
-#ifndef PRINT
- #if ORX_PRINT_DEBUG_OUTPUT
-  #define PRINT(x) \
-   PRINT ## x
-
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_ERROR
-   #define PRINT1  \
-    if (getSoftDebugLevel() >= ORX_ERROR)  \
-     PRINT_EXEC
-  #else
-   #define PRINT1 if (ORX_NONE) PRINT_EXEC
-  #endif
-
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_WARNING
-   #define PRINT2 \
-    if (getSoftDebugLevel() >= ORX_WARNING) \
-     PRINT_EXEC
-  #else
-   #define PRINT2 if (ORX_NONE) PRINT_EXEC
-  #endif
-
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_INFO
-   #define PRINT3 \
-    if (getSoftDebugLevel() >= ORX_INFO) \
-     PRINT_EXEC
-  #else
-   #define PRINT3 if (ORX_NONE) PRINT_EXEC
-  #endif
-
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_DEBUG
-   #define PRINT4 \
-    if (getSoftDebugLevel() >= ORX_DEBUG) \
-     PRINT_EXEC
-  #else
-   #define PRINT4 if (ORX_NONE) PRINT_EXEC
-  #endif
-
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_vDEBUG
-   #define PRINT5 \
-    if (getSoftDebugLevel() >= ORX_vDEBUG) \
-     PRINT_EXEC
-  #else
-   #define PRINT5 if (ORX_NONE) PRINT_EXEC
-  #endif
-
- #else /* if ORX_PRINT_DEBUG_OUTPUT */
-  #define PRINT(x) if (ORX_NONE) PRINT_EXEC
- #endif /* if ORX_PRINT_DEBUG_OUTPUT */
-
- #define PRINT0 \
-  PRINT_EXEC
-#endif /* ifndef PRINT */
-
+#define COUT_EXEC(x) orxonox::OutputHandler::getOutStream().setOutputLevel(x)
 
 ////////////////////////////////////////////////////////
 ///  COUT: just prints output as is with std::cout   ///
@@ -198,7 +67,8 @@ extern "C" _CoreExport int getSoftDebugLevel();
 #define COUTORX_WARNING COUT2
 #define COUTORX_INFO    COUT3
 #define COUTORX_DEBUG   COUT4
-#define COUTORX_vDEBUG  COUT5
+#define COUTORX_VERBOSE COUT5
+#define COUTORX_ULTRA   COUT6
 
 #ifndef COUT
  #if ORX_PRINT_DEBUG_OUTPUT
@@ -210,8 +80,8 @@ extern "C" _CoreExport int getSoftDebugLevel();
     if (getSoftDebugLevel() >= ORX_NONE)  \
      COUT_EXEC(0)
   #else
-   #define COUT0 if (ORX_NONE)\
-    COUT_EXEC(0)
+   #define COUT0 \
+    false ? COUT_EXEC(0) : COUT_EXEC(0)
   #endif
 
   #if ORX_HARD_DEBUG_LEVEL >= ORX_ERROR
@@ -219,8 +89,8 @@ extern "C" _CoreExport int getSoftDebugLevel();
     if (getSoftDebugLevel() >= ORX_ERROR)  \
      COUT_EXEC(1)
   #else
-   #define COUT1 if (ORX_NONE)\
-    COUT_EXEC(1)
+   #define COUT1 \
+    false ? COUT_EXEC(1) : COUT_EXEC(1)
   #endif
 
   #if ORX_HARD_DEBUG_LEVEL >= ORX_WARNING
@@ -228,8 +98,8 @@ extern "C" _CoreExport int getSoftDebugLevel();
     if (getSoftDebugLevel() >= ORX_WARNING) \
      COUT_EXEC(2)
   #else
-   #define COUT2 if (ORX_NONE) \
-    COUT_EXEC(2)
+   #define COUT2 \
+    false ? COUT_EXEC(2) : COUT_EXEC(2)
   #endif
 
   #if ORX_HARD_DEBUG_LEVEL >= ORX_INFO
@@ -237,8 +107,8 @@ extern "C" _CoreExport int getSoftDebugLevel();
     if (getSoftDebugLevel() >= ORX_INFO) \
      COUT_EXEC(3)
   #else
-   #define COUT3 if (ORX_NONE) \
-    COUT_EXEC(3)
+   #define COUT3 \
+    false ? COUT_EXEC(3) : COUT_EXEC(3)
   #endif
 
   #if ORX_HARD_DEBUG_LEVEL >= ORX_DEBUG
@@ -246,22 +116,31 @@ extern "C" _CoreExport int getSoftDebugLevel();
     if (getSoftDebugLevel() >= ORX_DEBUG) \
      COUT_EXEC(4)
   #else
-   #define COUT4 if (ORX_NONE) \
-    COUT_EXEC(4)
+   #define COUT4 \
+    false ? COUT_EXEC(4) : COUT_EXEC(4)
   #endif
 
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_vDEBUG
+  #if ORX_HARD_DEBUG_LEVEL >= ORX_VERBOSE
    #define COUT5 \
-    if (getSoftDebugLevel() >= ORX_vDEBUG) \
+    if (getSoftDebugLevel() >= ORX_VERBOSE) \
      COUT_EXEC(5)
   #else
-   #define COUT5 if (ORX_NONE) \
-    COUT_EXEC(5)
+   #define COUT5 \
+    false ? COUT_EXEC(5) : COUT_EXEC(5)
+  #endif
+
+  #if ORX_HARD_DEBUG_LEVEL >= ORX_ULTRA
+   #define COUT6 \
+    if (getSoftDebugLevel() >= ORX_ULTRA) \
+     COUT_EXEC(6)
+  #else
+   #define COUT6 \
+    false ? COUT_EXEC(6) : COUT_EXEC(6)
   #endif
 
  #else /* if ORX_PRINT_DEBUG_OUTPUT */
-  #define COUT(x) if (ORX_NONE) \
-   COUT_EXEC(5)
+  #define COUT(x) \
+    false ? COUT_EXEC(6) : COUT_EXEC(6)
  #endif /* if ORX_PRINT_DEBUG_OUTPUT */
 
 #endif /* ifndef COUT */
@@ -275,7 +154,8 @@ extern "C" _CoreExport int getSoftDebugLevel();
 #define CCOUTORX_WARNING CCOUT2
 #define CCOUTORX_INFO    CCOUT3
 #define CCOUTORX_DEBUG   CCOUT4
-#define CCOUTORX_vDEBUG  CCOUT5
+#define CCOUTORX_VERBOSE CCOUT5
+#define CCOUTORX_ULTRA   CCOUT6
 
 #define CCOUT_EXEC(x) \
   orxonox::OutputHandler::getOutStream().setOutputLevel(x) \
@@ -291,8 +171,8 @@ extern "C" _CoreExport int getSoftDebugLevel();
     if (getSoftDebugLevel() >= ORX_NONE)  \
      CCOUT_EXEC(0)
   #else
-   #define CCOUT0 if (ORX_NONE)\
-    CCOUT_EXEC(0)
+   #define CCOUT0 \
+    false ? CCOUT_EXEC(0) : CCOUT_EXEC(0)
   #endif
 
   #if ORX_HARD_DEBUG_LEVEL >= ORX_ERROR
@@ -300,8 +180,8 @@ extern "C" _CoreExport int getSoftDebugLevel();
     if (getSoftDebugLevel() >= ORX_ERROR)  \
      CCOUT_EXEC(1)
   #else
-   #define CCOUT1 if (ORX_NONE)\
-    CCOUT_EXEC(1)
+   #define CCOUT1 \
+    false ? CCOUT_EXEC(1) : CCOUT_EXEC(1)
   #endif
 
   #if ORX_HARD_DEBUG_LEVEL >= ORX_WARNING
@@ -309,8 +189,8 @@ extern "C" _CoreExport int getSoftDebugLevel();
     if (getSoftDebugLevel() >= ORX_WARNING) \
      CCOUT_EXEC(2)
   #else
-   #define CCOUT2 if (ORX_NONE) \
-    CCOUT_EXEC(2)
+   #define CCOUT2 \
+    false ? CCOUT_EXEC(2) : CCOUT_EXEC(2)
   #endif
 
   #if ORX_HARD_DEBUG_LEVEL >= ORX_INFO
@@ -318,8 +198,8 @@ extern "C" _CoreExport int getSoftDebugLevel();
     if (getSoftDebugLevel() >= ORX_INFO) \
      CCOUT_EXEC(3)
   #else
-   #define CCOUT3 if (ORX_NONE) \
-    CCOUT_EXEC(3)
+   #define CCOUT3 \
+    false ? CCOUT_EXEC(3) : CCOUT_EXEC(3)
   #endif
 
   #if ORX_HARD_DEBUG_LEVEL >= ORX_DEBUG
@@ -327,22 +207,31 @@ extern "C" _CoreExport int getSoftDebugLevel();
     if (getSoftDebugLevel() >= ORX_DEBUG) \
      CCOUT_EXEC(4)
   #else
-   #define CCOUT4 if (ORX_NONE) \
-    CCOUT_EXEC(4)
+   #define CCOUT4 \
+    false ? CCOUT_EXEC(4) : CCOUT_EXEC(4)
   #endif
 
-  #if ORX_HARD_DEBUG_LEVEL >= ORX_vDEBUG
+  #if ORX_HARD_DEBUG_LEVEL >= ORX_VERBOSE
    #define CCOUT5 \
-    if (getSoftDebugLevel() >= ORX_vDEBUG) \
+    if (getSoftDebugLevel() >= ORX_VERBOSE) \
      CCOUT_EXEC(5)
   #else
-   #define CCOUT5 if (ORX_NONE) \
-    CCOUT_EXEC(5)
+   #define CCOUT5 \
+    false ? CCOUT_EXEC(5) : CCOUT_EXEC(5)
+  #endif
+
+  #if ORX_HARD_DEBUG_LEVEL >= ORX_ULTRA
+   #define CCOUT6 \
+    if (getSoftDebugLevel() >= ORX_ULTRA) \
+     CCOUT_EXEC(6)
+  #else
+   #define CCOUT6 \
+    false ? CCOUT_EXEC(6) : CCOUT_EXEC(6)
   #endif
 
  #else /* if ORX_PRINT_DEBUG_OUTPUT */
-  #define CCOUT(x) if (ORX_NONE) \
-   CCOUT_EXEC(5)
+  #define CCOUT(x) \
+   false ? CCOUT_EXEC(6) : CCOUT_EXEC(6)
  #endif /* if ORX_PRINT_DEBUG_OUTPUT */
 
 #endif /* ifndef CCOUT */
