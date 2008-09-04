1ef9ea8774c8cba3126d072d9bfb23ee8faafd34
scheusso <scheusso@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2008-09-04 07:43:54 +0000

    further adds/changes

---
+++ code/branches/network/src/network/Packet/Acknowledgement.cc
@@ -0,0 +1,49 @@
+#include "Acknowledgement.h"
+#include "Packet.h"
+#include "network/Host.h"
+
+namespace network {
+namespace packet {
+
+#define PACKET_FLAGS_ACK    0
+#define _PACKETID           0
+#define _ACKID              _PACKETID + sizeof(network::packet::ENUM::Type)
+  
+Acknowledgement::Acknowledgement( unsigned int id, int clientID )
+ : PacketContent()
+{
+  flags_ = flags_ | PACKET_FLAGS_ACK;
+  data_=new unsigned char[ getSize() ];
+  *(ENUM::Type *)&data_[ _PACKETID ] = ENUM::Acknowledgement;
+  *(unsigned int *)&data_[ _ACKID ] = id;
+  clientID_=clientID;
+}
+
+Acknowledgement::Acknowledgement( unsigned char *data, int clientID )
+  : PacketContent(data)
+{
+  clientID_=clientID;
+}
+
+Acknowledgement::~Acknowledgement()
+{
+}
+
+unsigned char *Acknowledgement::getData(){
+  return data_;
+}
+
+unsigned int Acknowledgement::getSize() const{
+  return _ACKID + sizeof(unsigned int);
+}
+
+bool Acknowledgement::process(){
+  return Host::ackGamestate(data_[_ACKID], clientID_);
+}
+
+unsigned int Acknowledgement::getAckID(){
+  return *(unsigned int *)&data_[ _ACKID ];
+}
+
+} //namespace packet
+} //namespace network
---
+++ code/branches/network/src/network/Packet/ClassID.cc
@@ -0,0 +1,65 @@
+#include "ClassID.h"
+#include "Packet.h"
+#include "core/CoreIncludes.h"
+#include <string>
+
+
+namespace network {
+namespace packet {
+
+#define PACKET_FLAGS_CLASSID  ENET_PACKET_FLAG_RELIABLE
+#define _PACKETID             0
+#define _CLASSID              _PACKETID + sizeof(ENUM::Type)
+#define _CLASSNAMELENGTH      _CLASSID + sizeof(unsigned int)
+#define _CLASSNAME            _CLASSNAMELENGTH + sizeof(classNameLength_)
+  
+  ClassID::ClassID( unsigned int classID, std::string className )
+ : PacketContent()
+{
+  flags_ = flags_ | PACKET_FLAGS_CLASSID;
+  classNameLength_=className.length()+1;
+  data_=new unsigned char[ getSize() ];
+  if(!data_)
+    return;
+  *(packet::ENUM::Type *)&data_[ _PACKETID ] = packet::ENUM::ClassID;
+  *(unsigned int *)&data_[ _CLASSID ] = classID;
+  *(unsigned int *)&data_[ _CLASSNAMELENGTH ] = classNameLength_;
+  memcpy( &data_[ _CLASSNAME ], (void *)className.c_str(), classNameLength_ );
+}
+
+ClassID::ClassID( unsigned char *data )
+  : PacketContent(data)
+{
+  memcpy( (void *)&classNameLength_, &data[ sizeof(ENUM::Type) + sizeof(int) ], sizeof(classNameLength_) );
+}
+
+ClassID::~ClassID()
+{
+}
+
+unsigned char *ClassID::getData(){
+  return data_;
+}
+
+unsigned int ClassID::getSize() const{
+  return sizeof(network::packet::ENUM::Type) + sizeof(int) + classNameLength_;
+}
+
+bool ClassID::process(){
+  orxonox::Identifier *id=ID( std::string((const char*)&data_[ sizeof(network::packet::ENUM::Type) ]) );
+  if(id==NULL)
+    return false;
+  id->setNetworkID( getClassID() );
+  return true;
+}
+
+unsigned int ClassID::getClassID(){
+  return *(unsigned int *)&data_[ _CLASSID ];
+}
+
+// unsigned int ClassID::getClassNameLength(){
+//   return *(unsigned int *)&data[ _CLASSNAMELENGTH ];
+// }
+
+} //namespace packet
+}//namespace network
