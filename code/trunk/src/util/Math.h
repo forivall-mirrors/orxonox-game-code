d60252f5b598b8d239d7b834d579258aa499bef5
adrfried <adrfried@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2008-09-27 20:04:01 +0000

    integer overflow fixed

--- code/trunk/src/util/Math.h
+++ code/trunk/src/util/Math.h
@@ -219,7 +219,7 @@ T interpolateSmooth(float time, const T& start, const T& end)
 */
 inline _UtilExport float rnd()
 {
-    return ((float)rand() / (RAND_MAX + 1));
+    return rand() / (RAND_MAX + 1.0);
 }
 
 /**
