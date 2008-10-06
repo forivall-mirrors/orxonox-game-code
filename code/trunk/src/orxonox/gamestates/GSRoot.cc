73cbc786fbfc07ffa0ed5fe30d9162e120bfe3b4
rgrieder <rgrieder@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2008-10-06 17:39:38 +0000

    Moved all Ogre related code from GSRoot to GSGraphics. You should now be able to start the gui, goto ioConsole, then start the gui again and load the level. gui --> level --> gui doesn't yet work. But I will not dig into that until our object hierarchy has been replaced.

--- code/trunk/src/orxonox/gamestates/GSRoot.cc
+++ code/trunk/src/orxonox/gamestates/GSRoot.cc
@@ -29,13 +29,9 @@
 #include "OrxonoxStableHeaders.h"
 #include "GSRoot.h"
 
-#include <OgreLogManager.h>
-#include <OgreRoot.h>
-
 #include "util/Exception.h"
 #include "util/Debug.h"
 #include "core/Factory.h"
-#include "core/ConfigFileManager.h"
 #include "core/ConfigValueIncludes.h"
 #include "core/CoreIncludes.h"
 #include "core/ConsoleCommand.h"
@@ -70,14 +66,12 @@ namespace orxonox
     GSRoot::GSRoot()
         : RootGameState("root")
         , settings_(0)
-        , ogreRoot_(0)
-        , ogreLogger_(0)
-        , graphicsEngine_(0)
         , tclBind_(0)
         , tclThreadManager_(0)
         , shell_(0)
     {
         RegisterRootObject(GSRoot);
+        setConfigValues();
     }
 
     GSRoot::~GSRoot()
@@ -86,26 +80,10 @@ namespace orxonox
 
     void GSRoot::setConfigValues()
     {
-        SetConfigValue(ogreConfigFile_,  "ogre.cfg").description("Location of the Ogre config file");
-        SetConfigValue(ogrePluginsFile_, "plugins.cfg").description("Location of the Ogre plugins file");
-        SetConfigValue(ogreLogFile_,     "ogre.log").description("Logfile for messages from Ogre. \
-                                                                 Use \"\" to suppress log file creation.");
-        SetConfigValue(ogreLogLevelTrivial_ , 5).description("Corresponding orxonox debug level for ogre Trivial");
-        SetConfigValue(ogreLogLevelNormal_  , 4).description("Corresponding orxonox debug level for ogre Normal");
-        SetConfigValue(ogreLogLevelCritical_, 2).description("Corresponding orxonox debug level for ogre Critical");
     }
 
     void GSRoot::enter()
     {
-#if ORXONOX_DEBUG_MODE == 1
-        ConfigFileManager::getInstance().setFile(CFT_Settings, "orxonox_d.ini");
-#else
-        ConfigFileManager::getInstance().setFile(CFT_Settings, "orxonox.ini");
-#endif
-
-        // do this after the previous call..
-        setConfigValues();
-
         // creates the class hierarchy for all classes with factories
         Factory::createClassHierarchy();
 
@@ -129,11 +107,6 @@ namespace orxonox
         // create a shell
         this->shell_ = new Shell();
 
-        setupOgre();
-
-        // initialise graphics engine. Doesn't load the render window yet!
-        graphicsEngine_ = new GraphicsEngine();
-
         // limit the main thread to the first core so that QueryPerformanceCounter doesn't jump
         // do this after ogre has initialised. Somehow Ogre changes the settings again (not through
         // the timer though).
@@ -156,17 +129,6 @@ namespace orxonox
     void GSRoot::leave()
     {
         // TODO: remove and destroy console commands
-
-        delete graphicsEngine_;
-
-        delete this->ogreRoot_;
-
-#if ORXONOX_PLATFORM == ORXONOX_PLATFORM_WIN32
-        // delete the ogre log and the logManager (since we have created it).
-        this->ogreLogger_->getDefaultLog()->removeListener(this);
-        this->ogreLogger_->destroyLog(Ogre::LogManager::getSingleton().getDefaultLog());
-        delete this->ogreLogger_;
-#endif
 
         delete this->shell_;
         delete this->tclThreadManager_;
@@ -202,11 +164,11 @@ namespace orxonox
         // Get the current process core mask
 	    DWORD procMask;
 	    DWORD sysMask;
-#if _MSC_VER >= 1400 && defined (_M_X64)
+#  if _MSC_VER >= 1400 && defined (_M_X64)
 	    GetProcessAffinityMask(GetCurrentProcess(), (PDWORD_PTR)&procMask, (PDWORD_PTR)&sysMask);
-#else
+#  else
 	    GetProcessAffinityMask(GetCurrentProcess(), &procMask, &sysMask);
-#endif
+#  endif
 
 	    // If procMask is 0, consider there is only one core available
 	    // (using 0 as procMask will cause an infinite loop below)
@@ -225,114 +187,5 @@ namespace orxonox
 	    // Set affinity to the first core
 	    SetThreadAffinityMask(GetCurrentThread(), threadMask);
 #endif
-    }
-
-    /**
-    @brief
-        Creates the Ogre Root object and sets up the ogre log.
-    */
-    void GSRoot::setupOgre()
-    {
-        COUT(3) << "Setting up Ogre..." << std::endl;
-
-        // TODO: LogManager doesn't work on oli platform. The why is yet unknown.
-#if ORXONOX_PLATFORM == ORXONOX_PLATFORM_WIN32
-        // create a new logManager
-        ogreLogger_ = new Ogre::LogManager();
-        COUT(4) << "Ogre LogManager created" << std::endl;
-
-        // create our own log that we can listen to
-        Ogre::Log *myLog;
-        if (this->ogreLogFile_ == "")
-            myLog = ogreLogger_->createLog("ogre.log", true, false, true);
-        else
-            myLog = ogreLogger_->createLog(this->ogreLogFile_, true, false, false);
-        COUT(4) << "Ogre Log created" << std::endl;
-
-        myLog->setLogDetail(Ogre::LL_BOREME);
-        myLog->addListener(this);
-#endif
-
-        // Root will detect that we've already created a Log
-        COUT(4) << "Creating Ogre Root..." << std::endl;
-
-        if (ogrePluginsFile_ == "")
-        {
-            COUT(2) << "Warning: Ogre plugins file set to \"\". Defaulting to plugins.cfg" << std::endl;
-            ModifyConfigValue(ogrePluginsFile_, tset, "plugins.cfg");
-        }
-        if (ogreConfigFile_ == "")
-        {
-            COUT(2) << "Warning: Ogre config file set to \"\". Defaulting to config.cfg" << std::endl;
-            ModifyConfigValue(ogreConfigFile_, tset, "config.cfg");
-        }
-        if (ogreLogFile_ == "")
-        {
-            COUT(2) << "Warning: Ogre log file set to \"\". Defaulting to ogre.log" << std::endl;
-            ModifyConfigValue(ogreLogFile_, tset, "ogre.log");
-        }
-
-        // check for config file existence because Ogre displays (caught) exceptions if not
-        std::ifstream probe;
-        probe.open(ogreConfigFile_.c_str());
-        if (!probe)
-        {
-            // create a zero sized file
-            std::ofstream creator;
-            creator.open(ogreConfigFile_.c_str());
-            creator.close();
-        }
-        else
-            probe.close();
-
-        ogreRoot_ = new Ogre::Root(ogrePluginsFile_, ogreConfigFile_, ogreLogFile_);
-
-#if 0 // Ogre 1.4.3 doesn't yet support setDebugOutputEnabled(.)
-#if ORXONOX_PLATFORM != ORXONOX_PLATFORM_WIN32
-        // tame the ogre ouput so we don't get all the mess in the console
-        Ogre::Log* defaultLog = Ogre::LogManager::getSingleton().getDefaultLog();
-        defaultLog->setDebugOutputEnabled(false);
-        defaultLog->setLogDetail(Ogre::LL_BOREME);
-        defaultLog->addListener(this);
-#endif
-#endif
-
-        COUT(3) << "Ogre set up done." << std::endl;
-    }
-
-    /**
-    @brief
-        Method called by the LogListener interface from Ogre.
-        We use it to capture Ogre log messages and handle it ourselves.
-    @param message
-        The message to be logged
-    @param lml
-        The message level the log is using
-    @param maskDebug
-        If we are printing to the console or not
-    @param logName
-        The name of this log (so you can have several listeners
-        for different logs, and identify them)
-    */
-    void GSRoot::messageLogged(const std::string& message,
-        Ogre::LogMessageLevel lml, bool maskDebug, const std::string& logName)
-    {
-        int orxonoxLevel;
-        switch (lml)
-        {
-        case Ogre::LML_TRIVIAL:
-            orxonoxLevel = this->ogreLogLevelTrivial_;
-            break;
-        case Ogre::LML_NORMAL:
-            orxonoxLevel = this->ogreLogLevelNormal_;
-            break;
-        case Ogre::LML_CRITICAL:
-            orxonoxLevel = this->ogreLogLevelCritical_;
-            break;
-        default:
-            orxonoxLevel = 0;
-        }
-        OutputHandler::getOutStream().setOutputLevel(orxonoxLevel)
-            << "Ogre: " << message << std::endl;
     }
 }
