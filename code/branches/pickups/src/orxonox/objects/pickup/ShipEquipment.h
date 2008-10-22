8d82ac51e32b7acb6c352670d94186e85ce93a87
dsommer <dsommer@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2008-10-22 20:30:59 +0000

    Adding Subclasses to ShipItem.h

---
+++ code/branches/pickups/src/orxonox/objects/pickup/ShipEquipment.h
@@ -0,0 +1,41 @@
+
+#ifndef _ShipEquipment_H__
+#define _ShipEquipment_H__
+#include <string>
+#include <multimap>
+
+
+/*
+multimap<std::string, Item*> equipment_;
+equipment_.insert(pair<std::string, Item*>("Weapon", new Item()));*/
+
+
+namespaces orxonox{
+
+	class _OrxonoxExport ShipEquipment{
+	public:
+		void AddItem(Shipitem toAddItem);
+		void RemoveItem(Shipitem toRemoveItem);
+		bool CheckifValid(Shipitem toBeChecked);
+		int
+	private:
+		std::multimap<std::string, Item*> Equipment;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- code/branches/pickups/src/orxonox/objects/pickup/ShipItem.h
+++ code/branches/pickups/src/orxonox/objects/pickup/ShipItem.h
@@ -5,29 +5,83 @@
 #include <map>
 #include "ShipEquipmentClasses.h"
 
-enum itemType{ //Diffrent Types of Items
-	
-		Usable,
-		Powerups,
-		Permanent
-		};
+
 
 namespace orxonox{
+Class _OrxonoxExport ShipItem{
 
+	enum itemType{ //Diffrent Types of Items
 
+			Useable,
+			Powerups,
+			Permanent
+			};
 
-
-Class _OrxonoxExport ShipItem : public SpaceShip{
-	
 	public:
 		itemType CheckType();
-	 
+
 	private:
-		itemType type; 
-		
-	
+		itemType type;
+	};
 
 
+/* Useables are Items the Player can choose when to activate and then show an instant action/effect
+ * or for a certain duration */
+Class _OrxonoxExport Useable : public ShipItem{
+
+
+	enum subItemTypeUseable{ //Diffrent Types of Items of the Type Useable
+			Rockets,
+			Repairkits,
+			Triggers,		//trigger events usable by the player (Quests f.exp.)
+			Boosters,
+			Shields,
+			Appearance,
+			};
+
+			public:
+				subItemTypeUseable CheckSubType();
+
+			private:
+				subItemTypeUseable subtype;
+};
+
+/* Permanents are Items, that effect the Player or his Ship from the instance he picks them up
+   until he drops them again */
+Class _OrxonoxExport Permanent : public ShipItem{
+
+	enum subItemTypePermanent{ //Diffrent Types of Items of the Type Permanent
+			Weapon,
+			Thrusters,
+			Shields,
+			Armor
+			};
+			public:
+				subItemTypePermanent CheckSubType();
+
+			private:
+				subItemTypePermanent subtype;
+};
+
+// Powerups effect the Player right after he picks them up for a certain Duration /or permanently.
+Class _OrxonoxExport Powerups : public ShipItem{
+
+	enum subItemTypePowerups{ //Diffrent Types of Items of the Type Powerups
+			Weapon,
+			Boosters,
+			Shields,
+			Repairkits,
+			Appearance,
+			Trigger
+			};
+			public:
+			subItemTypePowerups CheckSubType();
+
+			private:
+			subItemTypePowerups subtype;
+};
+
+}
 
 
 
