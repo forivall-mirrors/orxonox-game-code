753824bbdc74670a2ed1e99bc6810ac00d0d4e19
martisty <martisty@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2008-11-11 09:50:35 +0000

    fixed an error, but problems with proper colision detection @ offset 0

--- code/branches/physics/src/orxonox/objects/HelloBullet.cc
+++ code/branches/physics/src/orxonox/objects/HelloBullet.cc
@@ -33,8 +33,8 @@
 #include <OgreSceneManager.h>
 #include <OgreEntity.h>
 
-#include "ogreode/OgreOde_Core.h"
-#include "ogreode/OgreOdeGeometry.h"
+// #include "ogreode/OgreOde_Core.h"
+// #include "ogreode/OgreOdeGeometry.h"
 #include "util/Convert.h"
 #include "core/CoreIncludes.h"
 #include "core/ConfigValueIncludes.h"
@@ -71,7 +71,7 @@ namespace orxonox
         dynamicsWorld-> setGravity(btVector3(0,-10,0));
 
 
-        btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0,1,0),10);
+        btCollisionShape* groundShape = new btStaticPlaneShape(btVector3(0,1,0),1);
 
         btCollisionShape* fallShape = new btSphereShape(1);
 
