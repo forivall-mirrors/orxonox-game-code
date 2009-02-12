fe6a55a03e57242b9eaad20ca74be11f95aca06f
rgrieder <rgrieder@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2009-02-12 14:28:48 +0000

    Boost finding bugfix for versions less than 1.35

--- code/branches/buildsystem2/cmake/LibraryConfig.cmake
+++ code/branches/buildsystem2/cmake/LibraryConfig.cmake
@@ -165,12 +165,16 @@ FIND_PACKAGE_HANDLE_STANDARD_ARGS(TCL DEFAULT_MSG TCL_LIBRARY TCL_INCLUDE_PATH)
 ##### Boost #####
 # Expand the next statement if newer boost versions than 1.36.1 are released
 SET(Boost_ADDITIONAL_VERSIONS 1.37 1.37.0)
-# MSVC seems to be the only compiler requiring system and date_time
+# MSVC seems to be the only compiler requiring date_time
 IF(MSVC)
-  FIND_PACKAGE(Boost 1.34 REQUIRED thread filesystem date_time system)
+  FIND_PACKAGE(Boost 1.34 REQUIRED thread filesystem date_time)
 ELSE(MSVC)
-  FIND_PACKAGE(Boost 1.34 REQUIRED thread filesystem system)
+  FIND_PACKAGE(Boost 1.34 REQUIRED thread filesystem)
 ENDIF(MSVC)
+# Boost 1.35 and newer also need the 'System' library
+IF(NOT Boost_VERSION LESS 103500)
+  FIND_PACKAGE(Boost 1.35 REQUIRED system)
+ENDIF()
 
 ####### Static/Dynamic linking options ##########
 
