efd305263f0459a9e36bd24d0d502d59553e277c
scheusso <scheusso@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2009-03-04 11:35:50 +0000

    not needed anymore

--- code/branches/network/bin/irc.tcl
+++
@@ -1,6 +0,0 @@
-proc every {ms body} { eval $body; after $ms [list every $ms $body] }
-package require irk
-set conn [irk::connect orxonox $nickname]
-irk::join $conn #orxonox
-every 25 {}
-vwait termination
