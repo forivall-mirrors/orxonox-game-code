b9e05d243bb628702362b393727080d3f10f4e1c
rgrieder <rgrieder@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2009-03-29 22:14:36 +0000

    quote: "Fixed some bugs. Please do not break the code, when working with other people on one branch" Me too. CMake wasn't even able to make the project after r2853! The problem was the no -Wno-deprecated flag, which was set to all compilers. Lets examine: There are three files with compiler settings: BuildConfig.cmake, BuildConfigGCC.cmake and BuildConfigMSVC.cmake. Suppose you want to apply a GCC specific option, which one do you pick? BuildConfig.cmake, exactly, because that only concerns GCC in contrast to BuildConfigGCC.cmake! I still hope the dozens if not hundreds of hours of work spent on the build system to make it less of a mess weren't all that in vein...
    - Also re enabled the OPTION to show extended compiler warnings and explicitly set it to false (which it was before, just not visibly).

--- code/branches/gui/cmake/BuildConfig.cmake
+++ code/branches/gui/cmake/BuildConfig.cmake
@@ -89,7 +89,7 @@ CHECK_OGRE_PLUGINS(${OGRE_PLUGINS})
 
 ################ Compiler Config ################
 
-#OPTION(EXTRA_COMPILER_WARNINGS "Enable some extra warnings (heavily pollutes the output)")
+OPTION(EXTRA_COMPILER_WARNINGS "Enable some extra warnings (heavily pollutes the output)" FALSE)
 
 INCLUDE(FlagUtilities)
 
@@ -183,7 +183,6 @@ SET(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
 
 # Disable Boost auto linking completely
 ADD_COMPILER_FLAGS("-DBOOST_ALL_NO_LIB")
-ADD_COMPILER_FLAGS("-Wno-deprecated")
 
 # If no defines are specified, these libs get linked statically
 ADD_COMPILER_FLAGS("-DBOOST_ALL_DYN_LINK" WIN32 LINK_BOOST_DYNAMIC)
