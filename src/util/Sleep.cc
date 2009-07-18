ba6516d1ca88966db58de2944822099c6dfc9399
rgrieder <rgrieder@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2009-07-18 16:24:38 +0000

    Removed annoying warning on Windows.

--- src/util/Sleep.cc
+++ src/util/Sleep.cc
@@ -47,8 +47,8 @@ namespace orxonox
 {
     void usleep(unsigned long microseconds)
     {
-        if (microseconds < 1000)
-            COUT(2) << "Warning: Windows cannot sleep less than 1ms, ignoring" << std::endl;
+        //if (microseconds < 1000)
+        //    COUT(2) << "Warning: Windows cannot sleep less than 1ms, ignoring" << std::endl;
         Sleep(microseconds / 1000);
     }
 
