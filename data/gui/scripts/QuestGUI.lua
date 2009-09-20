8d021703f18a7da0e8c4baf1b4c90d7cd05fa0a7
dafrick <dafrick@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2009-09-20 15:09:20 +0000

    some minor changes...

--- data/gui/scripts/QuestGUI.lua
+++ data/gui/scripts/QuestGUI.lua
@@ -1,6 +1,4 @@
--- QuestGUI
-
-BasicGUI = require("BasicGUI")
+gui = require("BasicGUI")
 local P = BasicGUI:new() --inherit everything from the gui package
 if _REQUIREDNAME == nil then
     QuestGUI = P
@@ -11,28 +9,12 @@ end
 P.filename = "QuestGUI"
 P.layoutString = "QuestGUI.layout"
 
-local windowManager = CEGUI.WindowManager:getSingleton()
--- local playerManager = orxonox.PlayerManager
-local questManager = orxonox.QuestManager:getInstance()
-
-local quests = {}
-
--- overwrites load function of previous load function
-function P:load()
-    element = P.__index.load(P) -- calling load function of parent class with ourselves and do other stuff now
-    --win = winMgr:getWindow("orxonox/LevelListBox")
-    --print(win.getItemCount())
-    --item = winMgr:createWindow("TaharezLook/ListboxItem", "orxonox/LevelListBoxItem1")
-    --item:setText("HiHo")
-    --win:addItem(item)
-    --print(win:getItemCount())
-    return element
-end
-
 function P:show()
     self.window:show() -- TDO: Do this through parent...
     self.visible = true
 
+    local questManager = orxonox.QuestManager:getInstance()
+      
     local questsList = windowManager:getWindow("orxonox/QuestGUI/QuestsList")
 
     local window = questManager:getQuestGUI(P.filename)
