625c1eaf71f0fff45646182c2b43dfd53fe6f3d6
scheusso <scheusso@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2009-09-23 14:57:45 +0000

    removed lines students will have to reinsert again

--- code/branches/tutorial/src/orxonox/controllers/DroneController.cc
+++ code/branches/tutorial/src/orxonox/controllers/DroneController.cc
@@ -20,7 +20,7 @@
  *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  *   Author:
- *      Fabian 'x3n' Landau
+ *      Oli Scheuss
  *   Co-authors:
  *      ...
  *
@@ -38,7 +38,8 @@ namespace orxonox
         // Place your code here:
         // - make sure to register the object in the factory
         // - do any kind of initialisation
-        RegisterObject(DroneController);
+        
+        
         
         // this checks that our creator really is a drone
         // and saves the pointer to the drone for the controlling commands
@@ -54,13 +55,11 @@ namespace orxonox
     {
         // Place your code here:
         // - steering commands
-        static float totaltime = 0;
-        totaltime += dt;
         Drone *myDrone = static_cast<Drone*>(this->getControllableEntity());
-        if(totaltime<100)
-        {
-            myDrone->moveFrontBack( -sqrt(dt) );
-            myDrone->rotatePitch(-dt);
-        }
+        // you can use the following commands for steering
+        // - moveFrontBack, moveRightLeft, moveUpDown
+        // - rotatePitch, rotateYaw, rotateRoll
+        // - apply the to myDrone (e.g. myDrone->rotateYaw(..) )
+        
     }
 }
