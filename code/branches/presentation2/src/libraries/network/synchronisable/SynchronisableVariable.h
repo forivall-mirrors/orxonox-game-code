a1706e950a578b7e21338cf2ae6ad77203ba34df
landauf <landauf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2009-11-23 21:20:01 +0000

    fixed build

--- code/branches/presentation2/src/libraries/network/synchronisable/SynchronisableVariable.h
+++ code/branches/presentation2/src/libraries/network/synchronisable/SynchronisableVariable.h
@@ -34,7 +34,7 @@
 
 #include <cassert>
 #include <cstring>
-#include "Serialise.h"
+#include "util/Serialise.h"
 #include "util/TypeTraits.h"
 #include "core/GameMode.h"
 #include "network/synchronisable/NetworkCallbackManager.h"
