b61cc21d098bf0a5ffdb9a2e2353b2f3e035216a
rgrieder <rgrieder@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2009-12-21 12:49:34 +0000

    Deleting old branches.

--- code/branches/menu/data/gui/scripts/GUITools.lua
+++
@@ -1,6 +0,0 @@
-function openDecisionPopup( text, callbackPtr )
-    orxonox.CommandExecutor:execute("showGUI DecisionPopup false true")
---     showGUI("DecisionPopup", false, true)
-    DecisionPopup.setCallback(InGameMenu.callback)
-    DecisionPopup.setText(text)
-end
