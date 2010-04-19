526873a55816d3f6671975df25f76121653840d7
dafrick <dafrick@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2010-04-19 14:33:46 +0000

    Creating new pickup branch for me.

---
+++ code/branches/pickup5/data/gui/scripts/QuestGUI.lua
@@ -0,0 +1,20 @@
+-- QuestGUI.lua
+
+local P = createMenuSheet("QuestGUI")
+
+function P.show()
+    P.window:show() -- TODO: Do this through parent...
+    P.visible = true
+
+    local questManager = orxonox.QuestManager:getInstance()
+
+    local questsList = winMgr:getWindow("orxonox/QuestGUI/QuestsList")
+
+    local window = questManager:getQuestGUI(P.name)
+
+    questsList:addChildWindow(window)
+
+end
+
+return P
+
