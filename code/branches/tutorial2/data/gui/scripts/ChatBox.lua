90a4c617468bbb16fe29224af06526282097d992
dafrick <dafrick@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2010-09-29 12:49:26 +0000

    Fix to get rid of lua/cegui error.

--- code/branches/tutorial2/data/gui/scripts/ChatBox.lua
+++ code/branches/tutorial2/data/gui/scripts/ChatBox.lua
@@ -2,6 +2,11 @@
 
 local P = createMenuSheet("ChatBox")
 
+function P.onLoad()
+  local window = winMgr:getWindow("orxonox/ChatBox")
+  orxonox.GUIManager:subscribeEventHelper(window, "CloseClicked", P.name .. ".ChatBoxCloseButton_clicked")
+end
+
 function P.ChatBoxCloseButton_clicked(e)
     orxonox.ChatInputHandler:getInstance():deactivate()
 end
