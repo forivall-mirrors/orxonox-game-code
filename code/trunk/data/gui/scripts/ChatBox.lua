59446263339601e82451d00d0f57eabd9f277a40
dafrick <dafrick@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2010-11-04 09:57:19 +0000

    Reverting a change that I made some time ago. It turns out, that CEGUI doesn't like it (on some machines, expecially on tardis) if Events for a window are defined after the child windows are defined in the .layout file. So putting it above the child windows should be fine.

--- code/trunk/data/gui/scripts/ChatBox.lua
+++ code/trunk/data/gui/scripts/ChatBox.lua
@@ -2,11 +2,6 @@
 
 local P = createMenuSheet("ChatBox")
 
-function P.onLoad()
-    local window = winMgr:getWindow("orxonox/ChatBox")
-    orxonox.GUIManager:subscribeEventHelper(window, "CloseClicked", P.name .. ".ChatBoxCloseButton_clicked")
-end
-
 function P.ChatBoxCloseButton_clicked(e)
     orxonox.ChatInputHandler:getInstance():deactivate()
 end
