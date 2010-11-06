6f96bd64383dfb51f01f11ee040acbe025cc4b00
rgrieder <rgrieder@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2010-11-06 16:09:15 +0000

    Cosmetic changes.

--- code/branches/ois_update/src/libraries/core/PathConfig.cc
+++ code/branches/ois_update/src/libraries/core/PathConfig.cc
@@ -89,10 +89,9 @@ namespace orxonox
 #ifdef ORXONOX_PLATFORM_WINDOWS
         // get executable module
         TCHAR buffer[1024];
-        if (GetModuleFileName(NULL, buffer, 1024) == 0){
+        if (GetModuleFileName(NULL, buffer, 1024) == 0)
             ThrowException(General, "Could not retrieve executable path.");
-		}
-		
+
 #elif defined(ORXONOX_PLATFORM_APPLE)
         char buffer[1024];
         uint32_t path_len = 1023;
@@ -125,15 +124,13 @@ namespace orxonox
         buffer[ret] = 0;
 #endif
 
-        executablePath_ = bf::path(buffer);
-//#ifndef ORXONOX_PLATFORM_APPLE
-        executablePath_ = executablePath_.branch_path(); // remove executable name
-//#endif
-		
+        // Remove executable filename
+        executablePath_ = bf::path(buffer).branch_path();
+
         /////////////////////
         // SET MODULE PATH //
         /////////////////////
-		
+
         if (bf::exists(executablePath_ / "orxonox_dev_build.keep_me"))
         {
             COUT(1) << "Running from the build tree." << std::endl;
