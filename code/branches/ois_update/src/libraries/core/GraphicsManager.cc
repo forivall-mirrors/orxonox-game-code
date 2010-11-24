704657c140e88b5c4df0356ab0db350fbcbba850
youngk <youngk@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2010-11-24 13:39:14 +0000

    Added a hack to force the orxonox window to front on OS X.

--- code/branches/ois_update/src/libraries/core/GraphicsManager.cc
+++ code/branches/ois_update/src/libraries/core/GraphicsManager.cc
@@ -313,7 +313,16 @@ namespace orxonox
         this->ogreWindowEventListener_->windowResized(renderWindow_);
 
         Ogre::WindowEventUtilities::addWindowEventListener(this->renderWindow_, ogreWindowEventListener_.get());
-
+		
+// HACK
+#ifdef __APPLE_CC__
+        //INFO: This will give our window focus, and not lock it to the terminal
+        ProcessSerialNumber psn = {0, kCurrentProcess};
+        TransformProcessType(&psn, kProcessTransformToForegroundApplication);
+        SetFrontProcess(&psn);
+#endif
+// End of HACK
+		
         // create a full screen default viewport
         // Note: This may throw when adding a viewport with an existing z-order!
         //       But in our case we only have one viewport for now anyway, therefore
