656846960c57722db53e6a055b8bc6f4f41bb736
landauf <landauf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2011-02-12 10:30:33 +0000

    removed some spaces

--- code/trunk/src/orxonox/sound/BaseSound.cc
+++ code/trunk/src/orxonox/sound/BaseSound.cc
@@ -108,7 +108,7 @@ namespace orxonox
             // Get a no source ID
             this->audioSource_ += 123455;
             while (alIsSource(++this->audioSource_));
-            
+
             return true; // sound source destroyed - return true
         }
         return false; // nothing done - return false
