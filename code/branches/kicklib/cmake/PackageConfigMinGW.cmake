b87d06a3bf5bf29a341d2db45f9c804e95ccac1d
landauf <landauf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2011-03-13 14:54:54 +0000

    adjusted a few library names and search paths for mingw dependencies

--- code/branches/kicklib/cmake/PackageConfigMinGW.cmake
+++ code/branches/kicklib/cmake/PackageConfigMinGW.cmake
@@ -47,6 +47,6 @@ IF(MINGW)
   # Certain find scripts don't behave as ecpected to we have
   # to specify the libraries ourselves.
   SET(TCL_LIBRARY  ${DEP_BINARY_DIR}/tcl85.dll CACHE FILEPATH "")
-  SET(ZLIB_LIBRARY ${DEP_BINARY_DIR}/zlib1.dll CACHE FILEPATH "")
+  SET(ZLIB_LIBRARY ${DEP_BINARY_DIR}/libzlib.dll CACHE FILEPATH "")
 
 ENDIF(MINGW)
