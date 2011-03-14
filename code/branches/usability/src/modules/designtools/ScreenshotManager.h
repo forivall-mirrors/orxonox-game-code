ef5aa1dc7e7a41cde8d87bcdc019e74203fa39f3
dafrick <dafrick@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2011-03-14 16:36:36 +0000

    Adding screenshot to KeybindMenu and making "size" of screenhsot generated by printScreenshotHD adjustable.

--- code/branches/usability/src/modules/designtools/ScreenshotManager.h
+++ code/branches/usability/src/modules/designtools/ScreenshotManager.h
@@ -39,27 +39,37 @@ namespace orxonox
         ScreenshotManager();
         ~ScreenshotManager();
 
-      /* Creates a screenshot with the given camera.
-        * @param camera Pointer to the camera "looking at" the scene of interest
-        * @param fileName the filename of the screenshot file.
-      */
+        /**
+         * @briefCreates a screenshot with the given camera.
+         * @param camera Pointer to the camera "looking at" the scene of interest
+         * @param fileName the filename of the screenshot file.
+         */
         void makeScreenshot() const;
 
-        static void makeScreenshot_s()
-            { getInstance().makeScreenshot(); }
+        //static void makeScreenshot_s()
+        //    { getInstance().makeScreenshot(); }
+        static void makeScreenshot_s(unsigned int size)
+            { getInstance().setGridSize(size); getInstance().makeScreenshot(); }
+
+        void setGridSize(unsigned int size)
+        {
+            this->mGridSize_ = size;
+            uint8_t* data_ = new uint8_t[(this->mWindowWidth_ * this->mGridSize_) * (this->mWindowHeight_ * this->mGridSize_) * 3];
+            this->mFinalPicturePB_ = Ogre::PixelBox(this->mWindowWidth_ * this->mGridSize_, this->mWindowHeight_ * this->mGridSize_, 1, Ogre::PF_B8G8R8, data_);
+        }
 
     protected:
         static std::string getTimestamp();
 
-        std::string    mFileExtension;
-        unsigned int   mGridSize, mWindowWidth, mWindowHeight;
-        bool           mDisableOverlays;
+        std::string    mFileExtension_;
+        unsigned int   mGridSize_, mWindowWidth_, mWindowHeight_;
+        bool           mDisableOverlays_;
         //temp texture with current screensize
-        Ogre::TexturePtr mTempTex;
-        Ogre::RenderTexture* mRT;
-        Ogre::HardwarePixelBufferSharedPtr mBuffer;
+        Ogre::TexturePtr mTempTex_;
+        Ogre::RenderTexture* mRT_;
+        Ogre::HardwarePixelBufferSharedPtr mBuffer_;
         //PixelBox for a large Screenshot, if grid size is > 1
-        Ogre::PixelBox  mFinalPicturePB;
+        Ogre::PixelBox  mFinalPicturePB_;
         uint8_t* data_;
 
         static ScreenshotManager* singletonPtr_s;
