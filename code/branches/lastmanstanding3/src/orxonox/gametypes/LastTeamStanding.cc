91961fc0cb2fcf271c9ad2ada3c5e80284955b69
jo <jo@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2011-04-03 16:05:20 +0000

    test.

--- code/branches/lastmanstanding3/src/orxonox/gametypes/LastTeamStanding.cc
+++ code/branches/lastmanstanding3/src/orxonox/gametypes/LastTeamStanding.cc
@@ -239,24 +239,24 @@ namespace orxonox
             if (it->first->getClientID() == CLIENTID_UNKNOWN)
                 continue;
 
-            if (it->second > 0)
+            if (it->second > 0)//a player that is alive
             {
-             //which party has survived?
+                //which party has survived?
                 std::map<PlayerInfo*, int>::iterator it2 = this->teamnumbers_.find(it->first);
                 if (it2 != this->teamnumbers_.end())
                 {
                     party = it2->second;
                 }
                 //if (party < 0) return; //if search failed
-             //victory message to all team members
+                //victory message to all team members, loose message to everyone else
                 for (std::map<PlayerInfo*, int>::iterator it3 = this->teamnumbers_.begin(); it3 != this->teamnumbers_.end(); ++it3)
                 {
                     if (it3->first->getClientID() == CLIENTID_UNKNOWN)
                         continue;
                     if (it3->second == party)
-                        this->gtinfo_->sendAnnounceMessage("You have won the match!", it3->first->getClientID());
+                        {this->gtinfo_->sendAnnounceMessage("You have won the match!", it3->first->getClientID());}
                     else
-                        this->gtinfo_->sendAnnounceMessage("You have lost the match!", it3->first->getClientID());
+                        {this->gtinfo_->sendAnnounceMessage("You have lost the match!", it3->first->getClientID());}
                 }
                 return;
             }
