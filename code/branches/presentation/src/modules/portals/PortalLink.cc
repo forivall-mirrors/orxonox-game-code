e3408ba2f4b4f343c1b3fb66ec1b90d16e671055
dafrick <dafrick@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2011-05-26 14:35:46 +0000

    Merging portals2 branch into presentation branch.

--- code/branches/presentation/src/modules/portals/PortalLink.cc
+++ code/branches/presentation/src/modules/portals/PortalLink.cc
@@ -1,3 +1,31 @@
+/*
+ *   ORXONOX - the hottest 3D action shooter ever to exist
+ *                    > www.orxonox.net <
+ *
+ *
+ *   License notice:
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *   Author:
+ *      Andreas Büchel
+ *   Co-authors:
+ *      ...
+ *
+ */
+
 #include "PortalLink.h"
 #include "core/XMLPort.h"
 #include "objects/triggers/MultiTriggerContainer.h"
@@ -16,6 +44,7 @@ namespace orxonox
     
     PortalLink::~PortalLink()
     {
+        
     }
     
     void PortalLink::XMLPort(Element& xmlelement, XMLPort::Mode mode)
@@ -36,17 +65,14 @@ namespace orxonox
     {
         if(entrance == 0)
         {
-            // TODO COUT
             return;
         }
         
-        std::map<PortalEndPoint *, PortalEndPoint *>::iterator endpoint = PortalLink::links_s.find(entrance);
+        std::map<PortalEndPoint *, PortalEndPoint *>::iterator endpoints = PortalLink::links_s.find(entrance);
         
-        if(endpoint == PortalLink::links_s.end())  // entrance has no corresponding exit
+        if(endpoints == PortalLink::links_s.end())  // entrance has no corresponding exit
             return;
-        
-        endpoint->second->jumpOut(entity);
+
+        endpoints->second->jumpOut(entity);
     }
-
-
 }
