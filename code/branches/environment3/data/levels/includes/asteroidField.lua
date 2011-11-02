4ca56980221665f72cbfeba42a9fd92d9c7d9388
fmauro <fmauro@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2011-11-02 14:35:23 +0000

    Added newtonian gravity to asteriods and comments

--- code/branches/environment3/data/levels/includes/asteroidField.lua
+++ code/branches/environment3/data/levels/includes/asteroidField.lua
@@ -1,20 +1,28 @@
-function asteroidField(posX,posY,posZ,radius,count)
+--[[ asteroid field generator
+--[[ 
+--[[ generates asteroid field
+--[[ posX, posY, posZ - position in space
+--[[ minSize, maxSize - size boundaries of each asteroid
+--[[ radius - size of the cube around position in space
+--[[ count - number of asteroids
+function asteroidField(posX, posY, posZ, minSize, maxSize, radius, count)
 	for i = 1, count, 1
 	do
-		size = math.random()
-		print("<MovableEntity ")
+		size = (math.random() * (maxSize - minSize)) + minSize
+		print("<StaticEntity ")
 		
 		print("position = \"") 
 		print((2 * math.random() * radius) - radius + posX) print(",") 
 		print((2 * math.random() * radius) - radius + posY) print(",") 
 		print((2 * math.random() * radius) - radius + posZ) print("\"")
 		
-		print("scale = \"") print(size * 150) print("\"")
+		print("scale = \"") print(size) print("\"")
 		
-		print("collisionType = dynamic linearDamping = 0.8 angularDamping = 0 collisiondamage = 1 enablecollisiondamage = true>")
-		print("<attached> <Model mass=\"") print(size * 1000) print("\" mesh=\"ast") print(math.mod(i,6) + 1) print(".mesh\" /> </attached>")
-		print("<collisionShapes> <SphereCollisionShape radius=\"") print(size * 350) print("\" /> </collisionShapes>")
-		print("</MovableEntity>")
+		print("collisionType = static linearDamping = 0.8 angularDamping = 0 collisiondamage = 1 enablecollisiondamage = true>")
+		print("<attached> <Model mass=\"") print(size * 10) print("\" mesh=\"ast") print(math.mod(i,6) + 1) print(".mesh\" /> </attached>")
+		print("<attached> <ForceField position=\"0,0,0\" mode=\"newtonianGravity\" diameter=\"") print(size + (size * 0.2)) print("\" massDiameter=\"") print(size) print("\" mass=\"") print(size * 1000) print("\" /> </attached>")
+		print("<collisionShapes> <SphereCollisionShape radius=\"") print(size) print("\" /> </collisionShapes>")
+		print("</StaticEntity>")
 	end
 	
 end
