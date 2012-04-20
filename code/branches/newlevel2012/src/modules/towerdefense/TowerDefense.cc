fc9602af15eb2e27adfca4dda7ce6c040a99c509
mentzerf <mentzerf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2012-04-20 13:00:32 +0000

    Added Gametype class

---
+++ code/branches/newlevel2012/src/modules/towerdefense/TowerDefense.cc
@@ -0,0 +1,17 @@
+/*
+ * TowerDefense.cpp
+ *
+ *  Created on: Apr 20, 2012
+ *      Author: mentzerf
+ */
+
+#include <towerdefense/TowerDefense.h>
+
+TowerDefense::TowerDefense() {
+	// TODO Auto-generated constructor stub
+
+}
+
+TowerDefense::~TowerDefense() {
+	// TODO Auto-generated destructor stub
+}
---
+++ code/branches/newlevel2012/src/modules/towerdefense/TowerDefense.h
@@ -0,0 +1,19 @@
+/*
+ * TowerDefense.h
+ *
+ *  Created on: Apr 20, 2012
+ *      Author: mentzerf
+ */
+
+#ifndef TOWERDEFENSE_H_
+#define TOWERDEFENSE_H_
+
+#include <Gametype.h>
+
+class TowerDefense: public orxonox::Gametype {
+public:
+	TowerDefense();
+	virtual ~TowerDefense();
+};
+
+#endif /* TOWERDEFENSE_H_ */
