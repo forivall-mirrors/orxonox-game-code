69bee31d5b3faa8b61dd0a45672bfd36323890f3
davidsa <davidsa@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2012-11-20 17:46:03 +0000

    Updated orxonox::Billboard so you can disable FrustumCulling. Also improved orxonox::LensFlare and added documentation.

--- code/branches/shaders/src/orxonox/graphics/LensFlare.h
+++ code/branches/shaders/src/orxonox/graphics/LensFlare.h
@@ -57,7 +57,7 @@ namespace orxonox
     //TODO: The Hardware Occlusion only works properly for a single Flare on the screen,
     // if we have multiple strong lights it'll become way more complicated to determine how much of every object is occluded individually
     // there's below a 100 render queue groups, so maybe we should take turns for each object to be tested, so we only have one of the objects rendered at a time
-    // obviously we shouldn't use too much of these effects anyways, since they use a lot of performance, so not sure whether it's worth implementing a solution that works for multiple lens flares on screen 
+    // obviously we shouldn't use too many of these effects anyways, since they use a lot of processing power, so not sure whether it's worth implementing a solution that works for multiple lens flares on screen 
     class _OrxonoxExport LensFlare : public StaticEntity, public Tickable
     {
         public:
@@ -66,8 +66,62 @@ namespace orxonox
             
             inline void setScale(float scale)
                 { this->scale_=scale; }
-            inline float getScale()
+            inline float getScale() const
                 { return this->scale_; }
+                
+            /**
+            @brief
+                This sets the resolution of the out-of-screen-fade-effect
+                
+                the higher the resolution, the smoother the transition, but it will also have a greater impact on the performance
+                this happens with O(n^2) since it's a two dimensional operation.
+            @param fadeResolution
+                how many point samples should be used per axis
+                
+                note: this will always be an odd number, so the center point is included in the samples
+            */
+            inline void setFadeResolution(unsigned int fadeResolution)
+                { this->fadeResolution_=fadeResolution>0?fadeResolution:1; }
+            /**
+            @brief
+                This returns the resolution of the out-of-screen-fade-effect
+            @return how many point samples are being used per axis
+            */
+            inline unsigned int getFadeResolution() const
+                { return this->fadeResolution_; }
+                
+            /**
+            @brief
+                This sets the exponent of the fade-out function
+            @param exponent
+                how strong should the fade-out effect be
+            */
+            inline void setFadeExponent(float exponent)
+                { this->fadeExponent_=exponent; }
+            /**
+            @brief
+                This returns the exponent of the fade-out function
+            @return the exponent of the fade-out function
+            */
+            inline float getFadeExponent() const
+                { return this->fadeExponent_; }
+                
+            /**
+            @brief
+               Turn the out-of-screen-fade-effect on or off
+            @param fadeOnViewBorder
+                true to turn the effect on, false to turn it off
+            */
+            inline void setFadeOnViewBorder(bool fadeOnViewBorder)
+                { this->fadeOnViewBorder_=fadeOnViewBorder; }
+            /**
+            @brief
+               Determine whether the out-of-screen-fade-effect is on or off
+            @return
+                true if the effect is on, false if it is off
+            */
+            inline bool isFadeOnViewBorder() const
+                { return this->fadeOnViewBorder_; }
 
             virtual void XMLPort(Element& xmlelement, XMLPort::Mode mode);
 
@@ -80,11 +134,18 @@ namespace orxonox
             
             void createBillboards();
             
-            void updateBillboardPositions();
+            void updateBillboardStates(unsigned int dimension, bool isLightVisible);
             
-            Billboard* occlusionBillboard_;
-            unsigned int cameraDistance_;
-            float scale_;
+            void updateBillboardAlphas(float alpha);
+            
+            unsigned int getPointCount(unsigned int dimension) const;
+            
+            Billboard* occlusionBillboard_;//!< this is a transparent billboard used solely for the Hardware Occlusion Query
+            unsigned int cameraDistance_;//!< current distance of the lensflare center from the camera
+            float scale_;//!< this factor is used to scale the billboard to the desired size
+            bool fadeOnViewBorder_;//!< should the effect fade out on the border of the view?
+            unsigned int fadeResolution_;//!< how many points should be sampled per axis for the screen border fade. High number => smooth fade, but uses more processing power
+            float fadeExponent_;//!< this determines how fast the flare fades away as it gets obstructed
     };
 }
 
