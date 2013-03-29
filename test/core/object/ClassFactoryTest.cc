2a2029022b537272b4671b96dd5c2fa47b86625b
landauf <landauf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2013-03-29 22:26:53 +0000

    added more tests

---
+++ test/core/object/ClassFactoryTest.cc
@@ -0,0 +1,17 @@
+#include <gtest/gtest.h>
+#include "core/object/ClassFactory.h"
+#include "core/BaseObject.h"
+
+namespace orxonox
+{
+    TEST(ClassFactoryTest, CanFabricateObject)
+    {
+        Factory* factory = new ClassFactory<BaseObject>("BaseObject");
+        OrxonoxClass* object = factory->fabricate(NULL);
+        ASSERT_TRUE(object != NULL);
+        BaseObject* baseObject = dynamic_cast<BaseObject*>(object);
+        EXPECT_TRUE(baseObject != NULL);
+        delete object;
+        // don't delete factory - it remains in the identifier
+    }
+}
