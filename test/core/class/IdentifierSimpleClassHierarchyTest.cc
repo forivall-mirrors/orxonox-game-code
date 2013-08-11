a9a3289434558c249816618261de40e3de9f8be4
landauf <landauf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2013-08-11 12:41:37 +0000

    added tests

---
+++ test/core/class/IdentifierSimpleClassHierarchyTest.cc
@@ -0,0 +1,81 @@
+#include <gtest/gtest.h>
+#include "core/CoreIncludes.h"
+#include "core/class/Identifiable.h"
+#include "core/class/OrxonoxClass.h"
+#include "core/class/OrxonoxInterface.h"
+
+namespace orxonox
+{
+    namespace
+    {
+        class Interface : public OrxonoxInterface
+            { public: Interface() { RegisterRootObject(Interface); } };
+
+        class BaseClass : public OrxonoxClass
+            { public: BaseClass() { RegisterRootObject(BaseClass); } };
+
+        class RealClass : public BaseClass, public Interface
+            { public: RealClass() { RegisterObject(RealClass); } };
+
+        // Fixture
+        class IdentifierSimpleClassHierarchyTest : public ::testing::Test
+        {
+            public:
+                virtual void SetUp()
+                {
+                    new ClassFactory<Interface>("Interface");
+                    new ClassFactory<BaseClass>("BaseClass");
+                    new ClassFactory<RealClass>("RealClass");
+                    IdentifierManager::createClassHierarchy();
+                }
+
+                virtual void TearDown()
+                {
+                    IdentifierManager::destroyAllIdentifiers();
+                }
+        };
+    }
+
+    TEST(IdentifierSimpleClassHierarchyTest_NoFixture, NoInitialization)
+    {
+        {
+            Identifier* identifier = Class(Interface);
+            EXPECT_EQ(0u, identifier->getChildren().size());
+            EXPECT_EQ(0u, identifier->getParents().size());
+        }
+        {
+            Identifier* identifier = Class(BaseClass);
+            EXPECT_EQ(0u, identifier->getChildren().size());
+            EXPECT_EQ(0u, identifier->getParents().size());
+        }
+        {
+            Identifier* identifier = Class(RealClass);
+            EXPECT_EQ(0u, identifier->getChildren().size());
+            EXPECT_EQ(0u, identifier->getParents().size());
+        }
+    }
+
+    TEST_F(IdentifierSimpleClassHierarchyTest, TestInterface)
+    {
+        Identifier* identifier = Class(Interface);
+
+        EXPECT_EQ(1u, identifier->getChildren().size());
+        EXPECT_EQ(0u, identifier->getParents().size());
+    }
+
+    TEST_F(IdentifierSimpleClassHierarchyTest, BaseClass)
+    {
+        Identifier* identifier = Class(BaseClass);
+
+        EXPECT_EQ(1u, identifier->getChildren().size());
+        EXPECT_EQ(0u, identifier->getParents().size());
+    }
+
+    TEST_F(IdentifierSimpleClassHierarchyTest, RealClass)
+    {
+        Identifier* identifier = Class(RealClass);
+
+        EXPECT_EQ(0u, identifier->getChildren().size());
+        EXPECT_EQ(2u, identifier->getParents().size());
+    }
+}
