b0ffbb1ede25cf587c190d8c81289236cd43961e
georgr <georgr@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2013-10-21 12:46:28 +0000

    Does not compile yet

---
+++ code/branches/wiimote/src/libraries/core/input/Wiimote_Test.h
@@ -0,0 +1,33 @@
+/*
+ * WiimoteTest.h
+ *
+ *  Created on: Oct 21, 2013
+ *      Author: georgr
+ */
+
+#ifndef WIIMOTETEST_H_
+#define WIIMOTETEST_H_
+
+#include <tools/interfaces/Tickable.h>
+#include "core/CoreIncludes.h"
+#include "core/command/ConsoleCommand.h"
+#include "InputState.h"
+
+namespace orxonox
+{
+	class Wiimote_Test
+	        : public WindowEventListener
+			, public Tickable
+			, public Singleton<Wiimote_Test>
+	{
+		friend class Singleton<Wiimote_Test>;
+		public:
+			void tick(float dt);
+			Wiimote_Test();
+			virtual ~Wiimote_Test();
+		private:
+			static Wiimote_Test* singletonPtr_s;
+
+	};
+}
+#endif /* WIIMOTETEST_H_ */
