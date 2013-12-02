a9a517cc7c88799c252a42ab9f8407f1380feb09
georgr <georgr@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2013-12-02 13:46:02 +0000

    trying to get a reaction out of the wiiMote

--- code/branches/wiimote/src/libraries/core/input/WiiMote.cc
+++ code/branches/wiimote/src/libraries/core/input/WiiMote.cc
@@ -20,13 +20,16 @@ namespace orxonox
 			if(p->Buttons.isPressed(CButtons::BUTTON_A))
 				orxout()<<"ZOMG!!1!"<<endl;
 			Orientation o;
+			p->Accelerometer.
 			p->Accelerometer.GetOrientation(o.pitch, o.roll, o.yaw);
-			int x = (int)((o.yaw-lastOrientation.yaw)*time.getDeltaTime());
-			int y = (int)((o.pitch-lastOrientation.pitch)*time.getDeltaTime());
+			int x = (int)(1000*(o.yaw-lastOrientation.yaw)/time.getDeltaTime());
+			int y = (int)(1000*(o.pitch-lastOrientation.pitch)/time.getDeltaTime());
 			IntVector2 abs(0, 0);
 			IntVector2 rel(x, y);
 			IntVector2 clippingSize(1920, 1080);
-			if(x!=0&&y!=0)
+			lastOrientation.pitch = o.pitch;
+			lastOrientation.yaw = o.yaw;
+			if(x!=0 && y!=0)
 			{
 				for (unsigned int i = 0; i < inputStates_.size(); ++i)
 								inputStates_[i]->mouseMoved(abs, rel, clippingSize);
