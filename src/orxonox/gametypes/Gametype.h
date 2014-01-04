272e874cad7e9134614d6a6396a12db8852c69b7
jo <jo@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2014-01-04 13:28:19 +0000

    Something went wrong. I will correct the error later. So far just this patch.

--- src/orxonox/gametypes/Gametype.h
+++ src/orxonox/gametypes/Gametype.h
@@ -39,7 +39,7 @@
 #include "core/class/SubclassIdentifier.h"
 #include "tools/interfaces/Tickable.h"
 #include "infos/GametypeInfo.h"
-#include "tools/Timer.h"
+//#include "tools/Timer.h"
 
 namespace orxonox
 {
@@ -158,7 +158,7 @@ namespace orxonox
             */
             inline unsigned int getNumberOfPlayers() const
                 { return this->players_.size(); }
-            void showMenu();
+            //void showMenu();
 
 
         protected:
@@ -175,7 +175,7 @@ namespace orxonox
 
             bool bAutoStart_;
             bool bForceSpawn_;
-            bool bAutoEnd_;
+            //bool bAutoEnd_;
 
             float time_;
             float timeLimit_;
@@ -198,7 +198,7 @@ namespace orxonox
             ConsoleCommand* dedicatedAddBots_;
             ConsoleCommand* dedicatedKillBots_;
             /* HACK HACK HACK */
-            Timer showMenuTimer_;
+            //Timer showMenuTimer_;
     };
 }
 
