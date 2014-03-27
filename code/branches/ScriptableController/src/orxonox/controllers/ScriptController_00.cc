96bb2b23f3324227acd6ade6cbd329f21ce683a2
mkronig <mkronig@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2014-03-27 15:13:16 +0000

    edited comments in levelTry.oxw file and scriptableController_00.cc

--- code/branches/ScriptableController/src/orxonox/controllers/ScriptController_00.cc
+++ code/branches/ScriptableController/src/orxonox/controllers/ScriptController_00.cc
@@ -1,29 +1,5 @@
 /*
- *   ORXONOX - the hottest 3D action shooter ever to exist
- *                    > www.orxonox.net <
- *
- *
- *   License notice:
- *
- *   This program is free software; you can redistribute it and/or
- *   modify it under the terms of the GNU General Public License
- *   as published by the Free Software Foundation; either version 2
- *   of the License, or (at your option) any later version.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program; if not, write to the Free Software
- *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
- *
- *   Author:
- *      Fabian 'x3n' Landau
- *   Co-authors:
- *      ...
- *
+First try of a scriptController. Target: An event occurs in the levelTry.oxw file, which is "heard" by an object of the type of this class. It then SHOULD (because it is not working) execute the party function.
  */
 
 #include "ScriptController_00.h"
@@ -35,12 +11,14 @@ namespace orxonox
 
     ScriptController_00::ScriptController_00(Context* context) : ArtificialController(context)
     {
+	//Working
         RegisterObject(ScriptController_00);
         orxout()<<"hello universe constructor"<< endl;
     }
 
     bool ScriptController_00::party(bool bTriggered, BaseObject* trigger)
        {
+	   //XMLPortEventSink seems not to execute the party function
            orxout()<<"hello universe party"<< endl;
            return true;
        }
@@ -49,6 +27,8 @@ namespace orxonox
     void ScriptController_00::XMLPort(Element& xmlelement, XMLPort::Mode mode)
     {
         SUPER(ScriptController_00, XMLPort, xmlelement, mode);
+
+	//WORKING
     	XMLPortEventSink(ScriptController_00, BaseObject, "party", party, xmlelement, mode);
         orxout()<<"hello universe xmlport"<< endl;
     }
