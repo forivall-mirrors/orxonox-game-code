fabfd9507ca33a58339b23b72a749f7817c25741
mkronig <mkronig@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2014-04-17 14:00:49 +0000

    tried to access a function of ScriptController. Failed. Put a ScriptController object in the levelTry xml file.

--- code/branches/ScriptableController/data/gui/scripts/testscript.lua
+++ code/branches/ScriptableController/data/gui/scripts/testscript.lua
@@ -1,5 +1,13 @@
-k = 1.2
+--k = 1.2
+
+--orxonox.execute("orxout message test " .. k)
+
+x = 1.1
+y = 2.2
+z = 3.3
 
 
-orxonox.execute("orxout message test " .. k)
+orxonox.ScriptController:moveToPosition(x, y, z)
+
+
 
