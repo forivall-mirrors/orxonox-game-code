af3a1c503fecb975370ec069e6036fad4f028c0f
mkronig <mkronig@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2014-05-22 13:39:37 +0000

    commit for merging files

--- code/branches/ScriptableController/data/gui/scripts/testscript.lua
+++ code/branches/ScriptableController/data/gui/scripts/testscript.lua
@@ -7,6 +7,10 @@ xl = -1000
 yl = 3000 
 zl = -1000
 
+xr = 2000
+yr = 500
+zr = 2000
+
 d = 5
 Tabs = 0
 
@@ -31,17 +35,48 @@ if ctrl ~= nil then
     ctrl:eventScheduler("mal", xl+3000*xt, yl+3000*yt, zl, xl, yl, zl, dt, t+0.9)
   end
 
+
   -- Update absolute time
   Tabs = math.pi + 0.9
+
+  
 
   -- Move away again, still looking at the station
   ctrl:eventScheduler("mal", 0,0,1000, xl,yl,zl, 3, Tabs+0.4)
 
+
+
   -- Update absolute time
   Tabs = Tabs + 0.4 + 3
 
+  
+
+  
+
   -- Transition the look from (xl,yl,zl) to (3500,0,0) in 3 seconds
-  ctrl:eventScheduler("chl", xl, yl, zl, 3500, 0, 0, 3, Tabs)
+  ctrl:eventScheduler("chl", xl, yl, zl, xr, yr, zr, 2, Tabs+0.2)
+
+  Tabs = Tabs + 2 + 0.2
+
+   ctrl:eventScheduler("mal", xr + 600,yr,zr, xr,yr,zr, 4, Tabs+0)
+
+   Tabs = Tabs + 4 
+
+  for t = 0,math.pi,dt do
+    xt = math.cos(t)
+    zt = math.sin(t)
+
+    ctrl:eventScheduler("mal", xr+600*xt, yr, zr+600*zt, xr, yr, zr, dt, Tabs+t)
+  end
+
+  Tabs = Tabs + math.pi
+
+  ctrl:eventScheduler("chl", xr, yr, zr, xl, yl, zl, 3, Tabs+0.2)
+
+  Tabs = Tabs + 3 + 0.2
+
+  ctrl:eventScheduler("mal", xm,ym,zm, xl,yl,zl, 4, Tabs+2.5)
+
 
 end
 
