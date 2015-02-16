48d149f2c507bdd49964fc6c7d983ad19a5a394d
landauf <landauf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2015-02-16 21:23:52 +0000

    removed old branches

--- code/branches/surfaceraceHS14/data/gui/scripts/MultiplayerOptionsMenu.lua
+++
@@ -1,9 +0,0 @@
--- MultiplayerOptionsMenu.lua
-
-local P = createMenuSheet("MultiplayerOptionsMenu")
-
-function P.MultiplayerOptionsBackButton_clicked(e)
-    hideMenuSheet(P.name)
-end
-
-return P
