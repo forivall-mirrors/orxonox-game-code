7deb3d2b8ef20ca2fcccabb3e5ee9a55154e9a8d
meggiman <meggiman@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2015-03-19 15:05:10 +0000

    Created header files for new classes GravityBombFire and GravityBomb

---
+++ code/branches/weaponFS15/src/modules/weapons/projectiles/GravityBomb.h
@@ -0,0 +1,64 @@
+/*
+ *   ORXONOX - the hottest 3D action shooter ever to exist
+ *                    > www.orxonox.net <
+ *
+ *
+ *   License notice:
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *   Author:
+ *      Gabriel Nadler
+ *   Co-authors:
+ *      simonmie
+ *
+ */
+
+
+/*
+ * GravityBomb.h
+ *
+ *  Created on: Mar 19, 2015
+ *      Author: meggiman
+ */
+
+#ifndef GravityBOMB_H_
+#define GravityBOMB_H_
+
+#include "weapons/WeaponsPrereqs.h"
+
+#include "tools/Timer.h"
+
+#include "graphics/ParticleSpawner.h"
+#include "interfaces/RadarViewable.h"
+
+#include "BasicProjectile.h"
+
+namespace orxonox
+{
+
+	class ConeCollisionShape;
+
+	class _WeaponsExport GravityBomb : public BasicProjectile , public RadarViewable, public MovableEntity
+	{
+		public:
+			GravityBomb(Context* context);
+
+		private:
+		float lifetime_;
+
+	}
+}
+#endif /* GravityBOMB_H_ */
---
+++ code/branches/weaponFS15/src/modules/weapons/weaponmodes/GravityBombFire.h
@@ -0,0 +1,63 @@
+/**
+ *   ORXONOX - the hottest 3D action shooter ever to exist
+ *                    > www.orxonox.net <
+ *
+ *
+ *   License notice:
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU General Public License
+ *   as published by the Free Software Foundation; either version 2
+ *   of the License, or (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program; if not, write to the Free Software
+ *   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ *   Author:
+ *      Oliver Scheuss
+ *   Co-authors:
+ *      ...
+ *
+ */
+
+/**
+    @file GravityBombFire.h
+    @brief Definition of the GravityBombFire class.
+*/
+
+#ifndef _GravityBombFire_H__
+#define _GravityBombFire_H__
+
+#include "weapons/WeaponsPrereqs.h"
+#include "weaponsystem/WeaponMode.h"
+
+namespace orxonox
+{
+
+    /**
+    @brief
+        Fires the GravityBomb
+    @author
+        Manuel Eggimann
+    @ingroup WeaponsWeaponModes
+    */
+    class _WeaponsExport GravityBombFire : public WeaponMode
+    {
+        public:
+            GravityBombFire(Context* context);
+            virtual ~GravityBombFire();
+
+            virtual void fire();
+
+        private:
+            float speed_; //!< The initial speed of the bomb when it is launched.
+    };
+}
+
+#endif /* _GravityBombFire_H__ */
