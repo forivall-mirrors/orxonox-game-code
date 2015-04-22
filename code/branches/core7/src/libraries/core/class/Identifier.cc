12f197ed26560f976bb9a2e2bc5ec10ef8229eba
landauf <landauf@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2015-04-22 20:32:04 +0000

    removed check from r10379 again

--- code/branches/core7/src/libraries/core/class/Identifier.cc
+++ code/branches/core7/src/libraries/core/class/Identifier.cc
@@ -54,7 +54,6 @@ namespace orxonox
     {
         this->factory_ = 0;
         this->bInitialized_ = false;
-        this->bRegistered_ = false;
         this->bLoadable_ = false;
         this->bIsVirtualBase_ = false;
 
