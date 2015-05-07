3c230503a009743d36d46746631f74d3e5074532
meggiman <meggiman@68c3cff9-7720-4a9a-8be7-ebb5feb604a2>
2015-05-07 15:43:36 +0000

    Added new models and fixed dammage bug. Bomb now fully working.

--- src/modules/weapons/projectiles/GravityBomb.h
+++ src/modules/weapons/projectiles/GravityBomb.h
@@ -64,6 +64,8 @@ namespace orxonox
 			virtual bool collidesAgainst(WorldEntity* otherObject, const btCollisionShape* cs, btManifoldPoint& contactPoint);
 			void detonate();
 		private:
+		static const float LIFETIME;
+
 		bool isDetonated_; //Used to check whether the Bomb has to be destroyed during next tick.
 		float timeToLife_; //Time the bomb flies before it explodes.
 
